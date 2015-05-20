93cac511270239839b944bf17fd3a27321953322
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-05-20 01:08:01 -0400

    py3k: Add __future__ imports on remaining files.
    These files are not affected by these options.

--- kraken/html.py
+++ kraken/html.py
@@ -1,5 +1,5 @@
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, division, print_function
 
 import dominate
 import regex
--- tests/test_html.py
+++ tests/test_html.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import, division, print_function
+
+
 class TestHtml(object):
 
     """
