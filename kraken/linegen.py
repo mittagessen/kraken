6996309dbd007f079d6a02240480f43f4bdf559c
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-12-15 21:40:36 +0100

    Header for linegen work

---
+++ kraken/linegen.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+linegen
+~~~~~~~
+
+An advanced line generation tool using a HarfBuzz for proper text shaping.
+"""
+
+from __future__ import absolute_import, division, print_function
+from future import standard_library
+standard_library.install_aliases()
+from builtins import range
+from builtins import object
+
+from pgi.repository import HarfBuzz as hb
