7e604a8a61848fd6f4f189bd0db9d79bd39d0085
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-12-21 11:25:48 +0100

    Add ketos command for training related tasks
    Split training related tasks like line generation, ground truth creation
    and training from the main kraken binary.

---
+++ kraken/ketos.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Benjamin Kiessling
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from __future__ import absolute_import, division, print_function
+from future import standard_library
+standard_library.install_aliases()
+
+import os
+import io
+import time
+import click
+import errno
+import unicodedata
+import numpy as np
+
+from PIL import Image
+from itertools import cycle
+from kraken import linegen
+
+from kraken.lib.exceptions import KrakenCairoSurfaceException
+
+APP_NAME = 'kraken'
+          
+spinner = cycle([u'⣾', u'⣽', u'⣻', u'⢿', u'⡿', u'⣟', u'⣯', u'⣷'])
+              
+def spin(msg):
+    click.echo(u'\r\033[?25l{}\t{}'.format(msg, next(spinner)), nl=False)
+
+
+@click.group()
+@click.option('-v', '--verbose', default=0, count=True)
+def cli(verbose):
+    ctx = click.get_current_context()
+    ctx.meta['verbose'] = verbose
+
+@cli.command('transcrib')
+@click.pass_context
+def transcription(ctx):
+    pass
+
+@cli.command('linegen')
+@click.pass_context
+@click.option('-f', '--font', default='sans', 
+              help='Font family to render texts in.')
+@click.option('-n', '--maxlines', type=click.INT, default=0,
+              help='Maximum number of lines to generate')
+@click.option('-e', '--encoding', default='utf-8', 
+              help='Decode text files with given codec.')
+@click.option('-u', '--normalization', 
+              type=click.Choice(['NFD', 'NFKD', 'NFC', 'NFKC']), default=None,
+              help='Normalize ground truth')
+@click.option('-fs', '--font-size', type=click.INT, default=32,
+              help='Font size to render texts in.')
+@click.option('-l', '--language', 
+              help='RFC-3066 language tag for language-dependent font shaping')
+@click.option('-r', '--rtl/--ltr', default=False,
+              help='Base horizontal text direction. The BiDi algorithm will '
+              'still apply.')
+@click.option('-v', '--vertical/--horizontal', 
+              help='Set text direction to Top-To-Bottom')
+@click.option('-m', '--mean', type=click.FLOAT, default=0.0,
+              help='Mean of distribution to take means for gaussian noise '
+              'from.')
+@click.option('-s', '--sigma', type=click.FLOAT, default=0.001,
+              help='Mean of distribution to take standard deviation values for '
+              'Gaussian noise from.')
+@click.option('-r', '--density', type=click.FLOAT, default=0.002,
+              help='Mean of distribution to take density values for S&P noise '
+              'from.')
+@click.option('-d', '--distort', type=click.FLOAT, default=1.0,
+              help='Mean of distribution to take distortion values from')
+@click.option('-ds', '--distortion_sigma', type=click.FLOAT, default=20.0,
+              help='Mean of distribution to take standard deviations for the '
+              'Gaussian kernel from')
+@click.option('-o', '--output', type=click.Path(), default='training_data',
+              help='Output directory')
+@click.argument('text', nargs=-1, type=click.Path(exists=True))
+def line_generator(ctx, font, maxlines, encoding, normalization, font_size, language,
+                   rtl, vertical, mean, sigma, density, distort, distortion_sigma,
+                   output, text):
+    """
+    Generates artificial text line training data.
+    """
+    lines = set()
+    if not text:
+        return
+    for t in text:
+        with click.open_file(t, encoding=encoding) as fp:
+            spin('Reading texts')
+            lines.update(fp.readlines())
+    if normalization:
+        lines = set([unicodedata.normalize(normalization, line) for line in lines])
+    click.secho(u'\b\u2713', fg='green', nl=False)
+    click.echo('\033[?25h\n', nl=False)
+    click.echo('Read {} unique lines'.format(len(lines)))
+    if maxlines and maxlines < len(lines):
+        click.echo('Sampling {} lines\t'.format(maxlines), nl=False)
+        lines = list(lines)
+        lines = [lines[idx] for idx in np.random.randint(0, len(lines), maxlines)]
+        click.secho(u'\u2713', fg='green')
+    try:
+        os.makedirs(output)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    for idx, line in enumerate(lines):
+        spin('Writing images')
+        try:
+            im = linegen.render_line(line, family=font, font_size=font_size,
+                                language=language, rtl=rtl, vertical=vertical)
+        except KrakenCairoSurfaceException as e:
+            click.secho(u'\b\u2717', fg='red')
+            click.echo('{}: {} {}'.format(e.message, e.width, e.height))
+            continue
+        im = linegen.degrade_line(im, mean, sigma, density)
+        im = linegen.distort_line(im, distort, distortion_sigma)
+        im.save('{}/{:06d}.png'.format(output, idx))
+        with open('{}/{:06d}.gt.txt'.format(output, idx), 'wb') as fp:
+            fp.write(line.encode('utf-8'))
+    click.secho(u'\b\u2713', fg='green', nl=False)
+    click.echo('\033[?25h\n', nl=False)
+
+if __name__ == '__main__':
+    cli()
