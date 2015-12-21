6385b1ffe0afea05abb24912e5f31d9259110456
Benjamin Kiessling <mittagessen@l.unchti.me>
2015-12-21 11:25:26 +0100

    Add KrakenCairoSurfaceException

--- kraken/lib/exceptions.py
+++ kraken/lib/exceptions.py
@@ -33,3 +33,21 @@ class KrakenRepoException(Exception):
 
     def __init__(self, message=None):
         Exception.__init__(self, message)
+
+
+class KrakenCairoSurfaceException(Exception):
+    """
+    Raised when the Cairo surface couldn't be created.
+
+    Attributes:
+        message (str): Error message
+        width (int): Width of the surface
+        height (int): Height of the surface
+    """
+    def __init__(self, message, width, height):
+        self.message = message
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        return repr(self.message)
