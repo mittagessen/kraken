976754d5de00693a6ec5c447aa6a70ec22aee51e
Benjamin Kiessling <mittagessen@l.unchti.me>
2016-10-19 23:29:37 +0200

    Minimal tests for output serialization

---
+++ tests/test_serialization.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import pickle
+import os
+
+from lxml import etree
+from io import StringIO
+from kraken import rpred
+from kraken import serialization
+
+thisfile = os.path.abspath(os.path.dirname(__file__))
+resources = os.path.abspath(os.path.join(thisfile, 'resources'))
+
+class TestSerializations(object):
+    """
+    Tests for output serialization
+    """
+    def setUp(self):
+        with open(os.path.join(resources, 'records.pkl')) as fp:
+            self.records = pickle.load(fp)
+
+    def test_hocr_serialization(self):
+        """
+        Test hOCR serialization
+        """
+        fp = StringIO()
+
+        fp.write(serialization.serialize(self.records, image_name='foo.png', template='hocr'))
+
+        doc = etree.fromstring(fp.getvalue())
+
+    def test_alto_serialization_validation(self):
+        """
+        Validates output against ALTO schema
+        """
+        fp = StringIO()
+
+        fp.write(serialization.serialize(self.records, image_name='foo.png', template='alto'))
+        doc = etree.fromstring(fp.getvalue())
+        print(fp.getvalue()[:2000])
+        with open(os.path.join(resources, 'alto-3-1.xsd')) as schema_fp:
+            alto_schema = etree.XMLSchema(etree.parse(schema_fp))
+            alto_schema.assertValid(doc)
