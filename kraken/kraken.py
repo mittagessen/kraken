mittagessen/kraken

commit 5eeba7079cf670134c98394bb6b6127634e722f7

Add vertical text support to page segmentation
and also to recognizer. correct calculation of coordinates in recognizer
output is still missing

kraken/kraken.py
@@ -19,7 +19,7 @@ from future import standard_library
 standard_library.install_aliases()
 
 import os
-import csv
+import json
 import click
 import time
 import tempfile
@@ -66,20 +66,19 @@ def binarizer(threshold, zoom, escale, border, perc, range, low, high, base_imag
     click.secho(u'\u2713', fg='green')
 
 
-def segmenter(scale, black_colseps, base_image, input, output):
+def segmenter(text_direction, scale, black_colseps, base_image, input, output):
     try:
         im = Image.open(input)
     except IOError as e:
         raise click.BadParameter(str(e))
     click.echo('Segmenting\t', nl=False)
     try:
-        res = pageseg.segment(im, scale, black_colseps)
+        res = pageseg.segment(im, text_direction, scale, black_colseps)
     except:
         click.secho(u'\u2717', fg='red')
         raise
-    with open_file(output, 'w') as fp:
-        for box in res:
-            fp.write(u'{},{},{},{}\n'.format(*box))
+    with open_file(output, 'wb') as fp:
+        json.dump(res, fp)
     click.secho(u'\u2713', fg='green')
 
 
@@ -94,8 +93,7 @@ def recognizer(model, pad, base_image, input, output, lines):
     if not lines:
         lines = input
     with open_file(lines, 'r') as fp:
-        bounds = [(int(x1), int(y1), int(x2), int(y2)) for x1, y1, x2, y2
-                  in csv.reader(fp)]
+        bounds = json.loads(fp)
         it = rpred.rpred(model, im, bounds, pad)
     preds = []
 
@@ -166,13 +164,16 @@ def binarize(threshold, zoom, escale, border, perc, range, low, high):
 
 
 @cli.command('segment')
+@click.option('-d', '--text-direction', default='horizontal-tb',
+               type=click.Choice(['horizontal-tb','vertical-lr', 'vertical-rl']),
+               help='Sets principal text direction')
 @click.option('--scale', default=None, type=click.FLOAT)
 @click.option('-b/-w', '--black_colseps/--white_colseps', default=False)
-def segment(scale=None, black_colseps=False):
+def segment(text_direction, scale, black_colseps):
     """
     Segments page images into text lines.
     """
-    return partial(segmenter, scale, black_colseps)
+    return partial(segmenter, text_direction, scale, black_colseps)
 
 
 @cli.command('ocr')

kraken/pageseg.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2015 Benjamin Kiessling
+#           2014 Thomas M. Breuel
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -309,7 +310,18 @@ def remove_hlines(binary, scale, maxsize=10):
     return np.array(labels != 0, 'B')
 
 
-def segment(im, scale=None, black_colseps=False):
+def rotate_lines(lines, angle, offset):
+    angle = np.radians(angle)
+    r = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+    p = np.array(lines).reshape((-1, 2))
+    offset = np.array([2*offset])
+    p = p.dot(r).reshape((-1, 4)).astype(int) + offset
+    x = np.sort(p[:,[0,2]])
+    y = np.sort(p[:,[1,3]])
+    return np.column_stack((x.flatten(), y.flatten())).reshape(-1, 4)
+
+
+def segment(im, text_direction='horizontal-lr', scale=None, black_colseps=False):
     """
     Segments a page into text lines.
 
@@ -318,6 +330,8 @@ def segment(im, scale=None, black_colseps=False):
 
     Args:
         im (PIL.Image): A bi-level page of mode '1' or 'L'
+        text_direction (str): Principal direction of the text
+                              (horizontal/vertical-lr/rl)
         scale (float): Scale of the image
         black_colseps (bool): Whether column separators are assumed to be
                               vertical black lines or not
@@ -327,11 +341,27 @@ def segment(im, scale=None, black_colseps=False):
                                 of the segmented lines in reading order.
 
     Raises:
-        KrakenInputException if the input image is not binarized
+        KrakenInputException if the input image is not binarized or the text
+        direction is invalid.
     """
 
     if im.mode != '1' and not is_bitonal(im):
         raise KrakenInputException('Image is not bi-level')
+
+    # rotate input image for vertical lines
+    if text_direction == 'horizontal-lr':
+        angle = 0
+    if text_direction == 'vertical-lr':
+        angle = 270
+        offset = (0, im.size[1])
+    elif text_direction == 'vertical-rl':
+        angle = 90
+        offset = (im.size[0], 0)
+    else:
+        raise KrakenInputException('Invalid text direction')
+
+    im = im.rotate(angle, expand=True)
+
     # honestly I've got no idea what's going on here. In theory a simple
     # np.array(im, 'i') should suffice here but for some reason the
     # tostring/fromstring magic in pil2array alters the array in a way that is
@@ -359,4 +389,5 @@ def segment(im, scale=None, black_colseps=False):
     order = reading_order([l.bounds for l in lines])
     lsort = topsort(order)
     lines = [lines[i].bounds for i in lsort]
-    return [(s2.start, s1.start, s2.stop, s1.stop) for s1, s2 in lines]
+    lines = [(s2.start, s1.start, s2.stop, s1.stop) for s1, s2 in lines]
+    return {'text_direction': text_direction, 'boxes':  rotate_lines(lines, 360-angle, offset).tolist()}

kraken/rpred.py
@@ -120,11 +120,15 @@ def extract_boxes(im, bounds):
     Yields:
         (PIL.Image) the extracted subimage
     """
-    for box in bounds:
-        if (box < (0, 0, 0, 0) or box[::2] > (im.size[0], im.size[0]) or
-           box[1::2] > (im.size[1], im.size[1])):
+    if bounds['text_direction'].startswith('vertical'):
+        angle = 90
+    else:
+        angle = 0
+    for box in bounds['boxes']:
+        if (box < [0, 0, 0, 0] or box[::2] > [im.size[0], im.size[0]] or
+           box[1::2] > [im.size[1], im.size[1]]):
             raise KrakenInputException('Line outside of image bounds')
-        yield im.crop(box), box
+        yield im.crop(box).rotate(angle, expand=True), box
 
 
 def dewarp(normalizer, im):
@@ -155,6 +159,10 @@ def rpred(network, im, bounds, pad=16, line_normalization=True, bidi_reordering=
     Args:
         network (kraken.lib.lstm.SegRecognizer): A SegRecognizer object
         im (PIL.Image): Image to extract text from
+        bounds (dict): A dictionary containing a 'boxes' entry with a list of
+                       coordinates (x0, y0, x1, y1) of a text line in the image
+                       and an entry 'text_direction' containing
+                       'horizontal/vertical-lr/rl'.
         bounds (iterable): An iterable returning a tuple defining the absolute
                            coordinates (x0, y0, x1, y1) of a text line in the
                            Image.
@@ -175,7 +183,7 @@ def rpred(network, im, bounds, pad=16, line_normalization=True, bidi_reordering=
 
     lnorm = getattr(network, 'lnorm', CenterNormalizer())
 
-    for box, coords in extract_boxes(im, bounds):
+    for box, coords in extract_boxes(im):
         # check if boxes are non-zero in any dimension
         if sum(coords[::2]) == False or coords[3] - coords[1] == False:
             yield ocr_record('', [], [])

commit e5a0f78b678e2e543f02d59155d095695ab98693

Aaand another regression from 5eeba707

kraken/kraken.py
@@ -91,7 +91,7 @@ def recognizer(model, pad, bidi_reordering, base_image, input, output, lines):
     if not lines:
         lines = input
     with open_file(lines, 'r') as fp:
-        bounds = json.loads(fp)
+        bounds = json.load(fp)
         it = rpred.rpred(model, im, bounds, pad, bidi_reordering)
     preds = []
 