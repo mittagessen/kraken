6f5e827833d71c57598e79673b0957552b780a58
Andreas Büttner <andbue@gmail.com>
2017-07-07 11:50:16 +0200

    New TlstmSeqRecognizer_legacy for compatibility with the ancient clstm version used in kraken

--- kraken/lib/tlstm.py
+++ kraken/lib/tlstm.py
@@ -20,7 +20,7 @@ class TBIDILSTM(nn.Module):
     """
     A torch module implementing a bidirectional LSTM with a linear layer as decoder.
     Very much the same as ocropy BIDILSTM but with logsoftmax instead of softmax at 
-    the output (needed for warp_ctc). The serialization format is the same as clstm's.
+    the output (needed for warp_ctc).
     """
     def __init__(self, ninp, nhid, nop):
         super(TBIDILSTM, self).__init__()
@@ -77,6 +77,7 @@ class TBIDILSTM(nn.Module):
 class TlstmSeqRecognizer(kraken.lib.lstm.SeqRecognizer):
     """
     Something like ClstmSeqRecognizer, using pytorch instead of clstm.
+    The serialization format is the same as the clstm/master branch.
     """
     def __init__(self, fname='', normalize=kraken.lib.lstm.normalize_nfkc, cuda=torch.cuda.is_available()):
         self.fname = fname
@@ -309,4 +310,171 @@ class TlstmSeqRecognizer(kraken.lib.lstm.SeqRecognizer):
         self.rnn.momentum = momentum
         self.optim = torch.optim.RMSprop(self.rnn.parameters(), lr=self.rnn.learning_rate, momentum=self.rnn.momentum)
         
+
+
+
+
+
+class TBIDILSTM_legacy(nn.Module):
+    """
+    A torch module implementing a bidirectional LSTM with a linear layer as decoder.
+    Very much the same as kraken clstm/separate-derivs BIDILSTM (hopefully). 
+    """
+    def __init__(self, ninp, nhid, nop):
+        super(TBIDILSTM_legacy, self).__init__()
+        
+        self.rnn = nn.LSTM(ninp+1, nhid, 1, bias=False, bidirectional=True)
+        self.decoder = nn.Linear(2*nhid, nop, bias=False)
+        self.softmax = nn.Softmax()
+        
+        self.init_weights()
+        
+        self.ninput = ninp
+        self.noutput = nop
+        self.nhidden = nhid
+        self.learning_rate = 0
+        self.momentum = 0
+        
+        
+    def init_weights(self, initrange=0.1):
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+        for p in self.rnn.parameters():
+            p.data.uniform_(-initrange, initrange)
+        
+        
+    def forward(self, inp, hidden):
+        leninp = len(inp)
+        cuda = True if inp.is_cuda else False
+        
+        ones1 = Variable(torch.ones(inp.size()[0], inp.size()[1], 1))
+        if cuda:
+            ones1 = ones1.cuda()
+        inp_oneup = torch.cat([ones1, inp], 2)
+
+        lstm_out, hidden = self.rnn(inp_oneup, hidden)
+
+        decoded = self.decoder(lstm_out.view(lstm_out.size(0)*lstm_out.size(1), lstm_out.size(2)) )
+        
+        decoded = self.softmax(decoded)
+        
+        return decoded.view(lstm_out.size(0), lstm_out.size(1), decoded.size(1)), hidden
+
+    
+    def init_hidden(self, bsz=1):
+        if self.rnn.weight_hh_l0.is_cuda:
+            return (Variable(torch.zeros(2, bsz, self.nhidden)).cuda(),
+                    Variable(torch.zeros(2, bsz, self.nhidden)).cuda())           
+        return (Variable(torch.zeros(2, bsz, self.nhidden)),
+                Variable(torch.zeros(2, bsz, self.nhidden)))
+    
+    
+
+class TlstmSeqRecognizer_legacy(kraken.lib.lstm.SeqRecognizer):
+    """
+    Something like ClstmSeqRecognizer, using pytorch instead of clstm.
+    It should behave like the kraken code built on clstm/separate-derivs.
+    Training is not supported. Needs testing!
+    """
+    def __init__(self, fname='', normalize=kraken.lib.lstm.normalize_nfkc, cuda=torch.cuda.is_available()):
+        self.fname = fname
+        self.rnn = None
+        self.normalize = normalize
+        self.cuda_available = cuda
+        if fname:
+            self._load_model()
+    
+    @classmethod
+    def init_model(cls, ninput, nhidden, noutput, codec, normalize=kraken.lib.lstm.normalize_nfkc, cuda=torch.cuda.is_available()):
+        self = cls()
+        self.codec = codec
+        self.normalize = normalize
+        self.rnn = TBIDILSTM_legacy(ninput, nhidden, noutput)
+        self.cuda_available = cuda
+        if self.cuda_available:
+            self.cuda()
+        return self
+    
+    def cuda(self):
+        if not self.cuda_available:
+            return 'CUDA not available!'
+        
+        self.rnn = self.rnn.cuda()    
+
+    def _load_model(self):
+        network = clstm_pb2.NetworkProto()
+        with open(self.fname, 'rb') as f:
+            network.ParseFromString(f.read())
+        
+        ninput = network.ninput
+        noutput = network.noutput
+        attributes = {a.key: a.value for a in network.attribute[:]}
+        self.kind = attributes['kind']
+        
+        # Codec
+        self.codec = kraken.lib.lstm.Codec()
+        code2char, char2code = {}, {}
+        for code, char in enumerate([126] + network.codec[1:]):
+            code2char[code] = chr(char)
+            char2code[chr(char)] = code
+        self.codec.code2char = code2char
+        self.codec.char2code = char2code
+        
+        # Networks
+        networks = {}
+        networks['softm'] = [n for n in network.sub[:] if n.kind == 'SoftmaxLayer'][0]
+        parallel = [n for n in network.sub[:] if n.kind == 'Parallel'][0]
+        networks['lstm1'] = [n for n in parallel.sub[:] if n.kind.startswith('NPLSTM')][0]
+        rev = [n for n in parallel.sub[:] if n.kind == 'Reversed'][0]
+        networks['lstm2'] = rev.sub[0]
+        
+        nhidden = int(networks['lstm1'].attribute[0].value)
+        
+        weights = {}
+        for n in networks:
+            weights[n] = {}
+            for w in networks[n].weights[:]:
+                weights[n][w.name] = np.array(w.value).reshape(w.dim[:])
+        self.weights = weights
+        
+        weightnames = ('.WGI', '.WGF', '.WCI', '.WGO')
+        # lstm
+        ih_hh_splits = torch.cat([torch.from_numpy(w.astype('float32')) \
+                                  for w in [weights['lstm1'][wn] \
+                                        for wn in weightnames]],0).split(ninput+1,1)
+        weight_ih_l0 = ih_hh_splits[0]
+        weight_hh_l0 = torch.cat(ih_hh_splits[1:], 1)
+        
+        # lstm_reversed
+        ih_hh_splits = torch.cat([torch.from_numpy(w.astype('float32')) \
+                                  for w in [weights['lstm2'][wn] \
+                                        for wn in weightnames]],0).split(ninput+1,1)
+        weight_ih_l0_rev = ih_hh_splits[0]
+        weight_hh_l0_rev = torch.cat(ih_hh_splits[1:], 1)
+        
+        # softmax
+        weight_softm = torch.from_numpy(weights['softm']['.W'].astype('float32'))
+
+        # attach weights
+        self.rnn = TBIDILSTM_legacy(ninput, nhidden, noutput)
+        self.rnn.rnn.weight_ih_l0 = nn.Parameter(weight_ih_l0)
+        self.rnn.rnn.weight_hh_l0 = nn.Parameter(weight_hh_l0)
+        self.rnn.rnn.weight_ih_l0_reverse = nn.Parameter(weight_ih_l0_rev)
+        self.rnn.rnn.weight_hh_l0_reverse = nn.Parameter(weight_hh_l0_rev)
+        self.rnn.decoder.weight = nn.Parameter(weight_softm)
+                
+        if self.cuda_available:
+            self.cuda()
+
+    def predictString(self, line):
+        line = Variable(torch.from_numpy(line.reshape(-1, 1, self.rnn.ninput).astype('float32')))
+        
+        if self.cuda_available:
+            line = line.cuda()
+        
+        out, _ = self.rnn.forward(line, self.rnn.init_hidden())
+        
+        #codes = self.translate_back(out)
+        codes = lstm.translate_back(out.cpu().contiguous().squeeze().data.numpy())
+        res = ''.join(self.codec.decode(codes))
+        return res.strip()
         
