954b073f2187a46a4e7f61aa76e185ab46be73b7
Benjamin Kiessling <mittagessen@l.unchti.me>
2018-05-22 15:35:46 +0200

    Adapt tests to new API

--- tests/test_rpred.py
+++ tests/test_rpred.py
@@ -8,10 +8,9 @@ import unittest
 from PIL import Image
 from nose.tools import raises
 
-from kraken.lib import lstm
+from kraken.lib.models import load_any
 from kraken.rpred import rpred
 from kraken.lib.exceptions import KrakenInputException
-
 
 thisfile = os.path.abspath(os.path.dirname(__file__))
 resources = os.path.abspath(os.path.join(thisfile, 'resources'))
@@ -32,5 +31,6 @@ class TestRecognition(unittest.TestCase):
         """
         Tests correct handling of invalid line coordinates.
         """
-        pred = rpred(None, self.im, {'boxes': [[-1, -1, 10000, 10000]], 'text_direction': 'horizontal'}, True)
+        nn = load_any(os.path.join(resources, 'toy.clstm'))
+        pred = rpred(nn, self.im, {'boxes': [[-1, -1, 10000, 10000]], 'text_direction': 'horizontal'}, True)
         next(pred)
