f9aecd76bfdb86bc2b607dd78fea39958a8d825d
Benjamin Kiessling <mittagessen@l.unchti.me>
2018-10-19 15:26:01 +0200

    only print format warning when output is actually jpg

--- kraken/kraken.py
+++ kraken/kraken.py
@@ -58,9 +58,11 @@ def binarizer(threshold, zoom, escale, border, perc, range, low, high, base_imag
         res = binarization.nlbin(im, threshold, zoom, escale, border, perc, range,
                                  low, high)
         form = None
-        if os.path.splitext(output)[1] in ['.jpg', '.jpeg', '.JPG', '.JPEG', '']:
+        ext = os.path.splitext(output)[1]
+        if ext in ['.jpg', '.jpeg', '.JPG', '.JPEG', '']:
             form = 'png'
-            logger.warning('jpeg does not support 1bpp images. Forcing to png.')
+            if ext:
+                logger.warning('jpeg does not support 1bpp images. Forcing to png.')
         res.save(output, format=form)
     except Exception:
         message('\u2717', fg='red')
