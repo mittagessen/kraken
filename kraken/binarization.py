53b5c02b08637c1b66c1892e3fe9a866b8c7a8d1
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-01-29 15:24:42 +0100

    Ensure binarized output is identical in size to original

--- kraken/binarization.py
+++ kraken/binarization.py
@@ -26,7 +26,7 @@ import numpy as np
 
 from PIL import Image
 from kraken.lib.util import pil2array, array2pil, is_bitonal, get_im_str
-from scipy.ndimage import filters, interpolation, morphology
+from scipy.ndimage import filters, interpolation, morphology, affine_transform
 
 from kraken.lib.exceptions import KrakenInputException
 
@@ -65,31 +65,34 @@ def nlbin(im: Image,
         KrakenInputException when trying to binarize an empty image.
     """
     im_str = get_im_str(im)
-    logger.info(u'Binarizing {}'.format(im_str))
+    logger.info('Binarizing {}'.format(im_str))
     if is_bitonal(im):
-        logger.info(u'Skipping binarization because {} is bitonal.'.format(im_str))
+        logger.info('Skipping binarization because {} is bitonal.'.format(im_str))
         return im
     # convert to grayscale first
-    logger.debug(u'Converting {} to grayscale'.format(im_str))
+    logger.debug('Converting {} to grayscale'.format(im_str))
     im = im.convert('L')
     raw = pil2array(im)
-    logger.debug(u'Scaling and normalizing')
+    logger.debug('Scaling and normalizing')
     # rescale image to between -1 or 0 and 1
     raw = raw/np.float(np.iinfo(raw.dtype).max)
     # perform image normalization
     if np.amax(raw) == np.amin(raw):
-        logger.warning(u'Trying to binarize empty image {}'.format(im_str))
+        logger.warning('Trying to binarize empty image {}'.format(im_str))
         raise KrakenInputException('Image is empty')
     image = raw-np.amin(raw)
     image /= np.amax(image)
 
-    logger.debug(u'Interpolation and percentile filtering')
+    logger.debug('Interpolation and percentile filtering')
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', UserWarning)
         m = interpolation.zoom(image, zoom)
         m = filters.percentile_filter(m, perc, size=(range, 2))
         m = filters.percentile_filter(m, perc, size=(2, range))
-        m = interpolation.zoom(m, 1.0/zoom)
+        mh, mw = m.shape
+        oh, ow = image.shape
+        scale = np.diag([mh * 1.0/oh, mw * 1.0/ow])
+        m = affine_transform(m, scale, output_shape=image.shape)
     w, h = np.minimum(np.array(image.shape), np.array(m.shape))
     flat = np.clip(image[:w, :h]-m[:w, :h]+1, 0, 1)
 
@@ -97,11 +100,11 @@ def nlbin(im: Image,
     d0, d1 = flat.shape
     o0, o1 = int(border*d0), int(border*d1)
     est = flat[o0:d0-o0, o1:d1-o1]
-    logger.debug(u'Threshold estimates {}'.format(est))
+    logger.debug('Threshold estimates {}'.format(est))
     # by default, we use only regions that contain
     # significant variance; this makes the percentile
     # based low and high estimates more reliable
-    logger.debug(u'Refine estimates')
+    logger.debug('Refine estimates')
     v = est-filters.gaussian_filter(est, escale*20.0)
     v = filters.gaussian_filter(v**2, escale*20.0)**0.5
     v = (v > 0.3*np.amax(v))
@@ -110,10 +113,9 @@ def nlbin(im: Image,
     est = est[v]
     lo = np.percentile(est.ravel(), low)
     hi = np.percentile(est.ravel(), high)
-
     flat -= lo
     flat /= (hi-lo)
     flat = np.clip(flat, 0, 1)
-    logger.debug(u'Thresholding at {}'.format(threshold))
+    logger.debug('Thresholding at {}'.format(threshold))
     bin = np.array(255*(flat > threshold), 'B')
     return array2pil(bin)
