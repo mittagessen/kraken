e61ab98a23ba9ed0a697b27d34b07b6634de5c71
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-04-04 15:59:41 +0200

    better test coverage for binarization

--- tests/test_binarization.py
+++ tests/test_binarization.py
@@ -5,8 +5,12 @@ from __future__ import absolute_import, division, print_function
 import unittest
 import os
 
+from nose.tools import raises
+
 from PIL import Image
 from kraken.binarization import nlbin
+
+from kraken.lib.exceptions import KrakenInputException
 
 thisfile = os.path.abspath(os.path.dirname(__file__))
 resources = os.path.abspath(os.path.join(thisfile, 'resources'))
@@ -16,11 +20,19 @@ class TestBinarization(unittest.TestCase):
     """
     Tests of the nlbin function for binarization of images
     """
-    def test_not_binarize_bw(self):
+    @raises(KrakenInputException)
+    def test_not_binarize_empty(self):
         """
         Test that mode '1' images aren't binarized again.
         """
         with Image.new('1', (1000,1000)) as im:
+            nlbin(im)
+
+    def test_not_binarize_bw(self):
+        """
+        Test that mode '1' images aren't binarized again.
+        """
+        with Image.open(os.path.join(resources, 'bw.png')) as im:
             self.assertEqual(im, nlbin(im))
 
     def test_binarize_no_bw(self):
