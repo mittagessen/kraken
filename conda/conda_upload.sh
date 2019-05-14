262048e39383a8202335e2f9dce6f4b798c75f3b
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-05-14 14:00:25 +0200

    only upload stable releases to anaconda

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -5,9 +5,9 @@ OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m.%d`
+export VERSION=$TRAVIS_TAG
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u mittagessen -l nightly $CONDA_BLD_PATH/$OS/kraken-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u mittagessen $CONDA_BLD_PATH/$OS/kraken-`date +%Y.%m.%d`-0.tar.bz2 --force
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
