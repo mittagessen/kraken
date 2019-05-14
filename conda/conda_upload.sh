c2dcad8c9a4acc8ddacf0d462f75aa8e944747e9
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-05-14 13:30:31 +0200

    actually build conda package

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda/conda-bld/linux-64/kraken-*.tar.bz2 --output-dir conda-bld/
-
-echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/kraken-*.tar.bz2
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m.%d`
+conda build .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u mittagessen -l nightly $CONDA_BLD_PATH/$OS/kraken-`date +%Y.%m.%d`-0.tar.bz2 --force
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
