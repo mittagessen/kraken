0abbfd65600bce8e573f484c231dcc9e399a51a9
Benjamin Kiessling <mittagessen@l.unchti.me>
2019-05-14 14:25:52 +0200

    try to upload correct version this time

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -7,7 +7,7 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=$TRAVIS_TAG
 conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u mittagessen $CONDA_BLD_PATH/$OS/kraken-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u mittagessen $CONDA_BLD_PATH/$OS/kraken-$VERSION-0.tar.bz2 --force
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
