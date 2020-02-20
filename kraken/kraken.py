563bf344b170ce39e1d11aa56f6aba9a5628c848
Benjamin Kiessling <mittagessen@l.unchti.me>
2020-02-20 17:41:13 +0100

    auto -x/-bl switch determination regression

--- kraken/kraken.py
+++ kraken/kraken.py
@@ -258,7 +258,7 @@ def binarize(threshold, zoom, escale, border, perc, range, low, high):
 @cli.command('segment')
 @click.pass_context
 @click.option('-i', '--model',
-              default=pkg_resources.resource_filename(__name__, 'blla.mlmodel'),
+              default=None,
               show_default=True, type=click.Path(exists=True),
               help='Baseline detection model to use')
 @click.option('-x/-bl', '--boxes/--baseline', default=True, show_default=True,
