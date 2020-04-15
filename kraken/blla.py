bea7031d014d7a1d371ae0e9e596556edaa6bfc6
Benjamin Kiessling <mittagessen@l.unchti.me>
2020-04-15 21:20:14 +0200

    print warning in blla.segment for incorrect image mode

--- kraken/blla.py
+++ kraken/blla.py
@@ -95,6 +95,12 @@ def segment(im,
     if model is None:
         logger.info('No segmentation model given. Loading default model.')
         model = vgsl.TorchVGSLModel.load_model(pkg_resources.resource_filename(__name__, 'blla.mlmodel'))
+
+    if model.one_channel_mode == '1' and not is_bitonal(im)
+        logger.warning('Running binary model on non-binary input image '
+                       '(mode {}). This will result in severely degraded '
+                       'performance'.format(im.mode))
+
     model.eval()
     model.to(device)
 
