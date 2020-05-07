shape calculation fix in reshape layer

kraken/lib/layers.py
@@ -180,7 +180,7 @@ class Reshape(Module):
     def get_shape(self, input: Tuple[int, int, int, int]) -> Tuple[int, int, int, int]:
         input_shape = torch.zeros([x if x else 1 for x in input])
         with torch.no_grad():
-            o = self.forward(input_shape)
+            o, _ = self.forward(input_shape)
         return tuple(o.shape)  # type: ignore
 
     def deserialize(self, name, spec):