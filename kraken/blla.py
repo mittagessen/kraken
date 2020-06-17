d477471522e8209d4ad7a7317d44610073f9d7a1
Benjamin Kiessling <mittagessen@l.unchti.me>
2020-06-17 17:49:43 +0200

    refactor segmentation to allow only computing regions/lines

--- kraken/blla.py
+++ kraken/blla.py
@@ -42,6 +42,101 @@ __all__ = ['segment']
 
 logger = logging.getLogger(__name__)
 
+def compute_segmentation_map(im,
+                             mask: Optional[np.array] = None,
+                             model=None,
+                             device: str = 'cpu'):
+    """
+
+    """
+    im_str = get_im_str(im)
+    logger.info(f'Segmenting {im_str}')
+
+    if model is None:
+        logger.info('No segmentation model given. Loading default model.')
+        model = vgsl.TorchVGSLModel.load_model(pkg_resources.resource_filename(__name__, 'blla.mlmodel'))
+
+    if model.one_channel_mode == '1' and not is_bitonal(im):
+        logger.warning('Running binary model on non-binary input image '
+                       '(mode {}). This will result in severely degraded '
+                       'performance'.format(im.mode))
+
+    model.eval()
+    model.to(device)
+
+    if mask:
+        if mask.mode != '1' and not is_bitonal(mask):
+            logger.error('Mask is not bitonal')
+            raise KrakenInputException('Mask is not bitonal')
+        mask = mask.convert('1')
+        if mask.size != im.size:
+            logger.error('Mask size {mask.size} doesn\'t match image size {im.size}')
+            raise KrakenInputException('Mask size {mask.size} doesn\'t match image size {im.size}')
+        logger.info('Masking enabled in segmenter.')
+        mask = pil2array(mask)
+
+    batch, channels, height, width = model.input
+    transforms = dataset.generate_input_transforms(batch, height, width, channels, 0, valid_norm=False)
+    res_tf = tf.Compose(transforms.transforms[:3])
+    scal_im = res_tf(im).convert('L')
+
+    with torch.no_grad():
+        logger.debug('Running network forward pass')
+        o, _ = model.nn(transforms(im).unsqueeze(0).to(device))
+    logger.debug('Upsampling network output')
+    o = F.interpolate(o, size=scal_im.size[::-1])
+    o = o.squeeze().cpu().numpy()
+    scale = np.divide(im.size, o.shape[:0:-1])
+    return {'heatmap': o, 'cls_map': model.user_metadata['class_mapping'], 'scale': scale, 'scal_im': scal_im}
+
+
+def vec_regions(heatmap: torch.Tensor, cls_map: Dict, scale: float, **kwargs):
+    """
+    Computes regions from a stack of heatmaps, a class mapping, and scaling
+    factor.
+    """
+    logger.info('Vectorizing regions')
+    regions = {}
+    for region_type, idx in cls_map['regions'].items():
+        logger.debug(f'Vectorizing regions of type {region_type}')
+        regions[region_type] = vectorize_regions(heatmap[idx])
+    for reg_id, regs in regions.items():
+        regions[reg_id] = scale_regions(regs, scale)
+    return regions
+
+
+def vec_lines(heatmap: torch.Tensor,
+              cls_map: Dict,
+              scale: float,
+              text_direction: str = 'horizontal-lr',
+              reading_order_fn: Callable = polygonal_reading_order,
+              regions: Dict = None,
+              scal_im = None,
+              **kwargs):
+    """
+    Computes lines from a stack of heatmaps, a class mapping, and scaling
+    factor.
+    """
+    st_sep = cls_map['aux']['_start_separator']
+    end_sep = cls_map['aux']['_end_separator']
+
+    logger.info('Vectorizing baselines')
+    baselines = []
+    for bl_type, idx in cls_map['baselines'].items():
+        logger.debug(f'Vectorizing lines of type {bl_type}')
+        baselines.extend([(bl_type,x) for x in vectorize_lines(heatmap[(st_sep, end_sep, idx), :, :])])
+    logger.debug('Polygonizing lines')
+    lines = list(filter(lambda x: x[2] is not None, zip([x[0] for x in baselines],
+                                                        [x[1] for x in baselines],
+                                                        calculate_polygonal_environment(scal_im, [x[1] for x in baselines]))))
+    logger.debug('Scaling vectorized lines')
+    sc = scale_polygonal_lines([x[1:] for x in lines], scale)
+    lines = list(zip([x[0] for x in lines], [x[0] for x in sc], [x[1] for x in sc]))
+    logger.debug('Reordering baselines')
+    lines = reading_order_fn(lines=lines, regions=regions, text_direction=text_direction[-2:])
+    return [{'script': bl_type, 'baseline': bl, 'boundary': pl} for bl_type, bl, pl in lines]
+
+
 def segment(im,
             text_direction: str = 'horizontal-lr',
             mask: Optional[np.array] = None,
@@ -94,79 +189,24 @@ def segment(im,
     im_str = get_im_str(im)
     logger.info(f'Segmenting {im_str}')
 
-    if model is None:
-        logger.info('No segmentation model given. Loading default model.')
-        model = vgsl.TorchVGSLModel.load_model(pkg_resources.resource_filename(__name__, 'blla.mlmodel'))
-
-    if model.one_channel_mode == '1' and not is_bitonal(im):
-        logger.warning('Running binary model on non-binary input image '
-                       '(mode {}). This will result in severely degraded '
-                       'performance'.format(im.mode))
-
-    model.eval()
-    model.to(device)
-
-    if mask:
-        if mask.mode != '1' and not is_bitonal(mask):
-            logger.error('Mask is not bitonal')
-            raise KrakenInputException('Mask is not bitonal')
-        mask = mask.convert('1')
-        if mask.size != im.size:
-            logger.error('Mask size {mask.size} doesn\'t match image size {im.size}')
-            raise KrakenInputException('Mask size {mask.size} doesn\'t match image size {im.size}')
-        logger.info('Masking enabled in segmenter.')
-        mask = pil2array(mask)
-
-    batch, channels, height, width = model.input
-    transforms = dataset.generate_input_transforms(batch, height, width, channels, 0, valid_norm=False)
-    res_tf = tf.Compose(transforms.transforms[:3])
-    scal_im = res_tf(im).convert('L')
-
-    with torch.no_grad():
-        logger.debug('Running network forward pass')
-        o, _ = model.nn(transforms(im).unsqueeze(0).to(device))
-    logger.debug('Upsampling network output')
-    o = F.interpolate(o, size=scal_im.size[::-1])
-    o = o.squeeze().cpu().numpy()
-    scale = np.divide(im.size, o.shape[:0:-1])
-    # postprocessing
-    cls_map = model.user_metadata['class_mapping']
-    st_sep = cls_map['aux']['_start_separator']
-    end_sep = cls_map['aux']['_end_separator']
-
-    logger.info('Vectorizing baselines')
-    baselines = []
-    regions = {}
-    for bl_type, idx in cls_map['baselines'].items():
-        logger.debug(f'Vectorizing lines of type {bl_type}')
-        baselines.extend([(bl_type,x) for x in vectorize_lines(o[(st_sep, end_sep, idx), :, :])])
-    logger.info('Vectorizing regions')
-    for region_type, idx in cls_map['regions'].items():
-        logger.debug(f'Vectorizing regions of type {region_type}')
-        regions[region_type] = vectorize_regions(o[idx])
-    logger.debug('Polygonizing lines')
-    lines = list(filter(lambda x: x[2] is not None, zip([x[0] for x in baselines],
-                                                        [x[1] for x in baselines],
-                                                        calculate_polygonal_environment(scal_im, [x[1] for x in baselines]))))
-    logger.debug('Scaling vectorized lines')
-    sc = scale_polygonal_lines([x[1:] for x in lines], scale)
-    lines = list(zip([x[0] for x in lines], [x[0] for x in sc], [x[1] for x in sc]))
-    logger.debug('Scaling vectorized regions')
-    for reg_id, regs in regions.items():
-        regions[reg_id] = scale_regions(regs, scale)
-    logger.debug('Reordering baselines')
-    order_regs = []
+    rets = compute_segmentation_map(im, mask, model, device)
+    regions = vec_regions(**rets)
+    # flatten regions for line ordering
+    line_regs = []
     for regs in regions.values():
-        order_regs.extend(regs)
-    lines = reading_order_fn(lines=lines, regions=order_regs, text_direction=text_direction[-2:])
+        line_regs.extend(regs)
+    lines = vec_lines(**rets,
+                      regions=line_regs,
+                      reading_order_fn=reading_order_fn,
+                      text_direction=text_direction)
 
-    if 'class_mapping' in model.user_metadata and len(model.user_metadata['class_mapping']['baselines']) > 1:
+    if len(rets['cls_map']['baselines']) > 1:
         script_detection = True
     else:
         script_detection = False
 
     return {'text_direction': text_direction,
             'type': 'baselines',
-            'lines': [{'script': bl_type, 'baseline': bl, 'boundary': pl} for bl_type, bl, pl in lines],
+            'lines': lines,
             'regions': regions,
             'script_detection': script_detection}
