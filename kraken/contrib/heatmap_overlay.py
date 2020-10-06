8faa96a55eeadc6c88008a20ca522ce2dc04c1b4
Benjamin Kiessling <mittagessen@l.unchti.me>
2020-10-06 13:09:35 +0200

    new bl segmentation heatmap overlay script

--- kraken/contrib/heatmap_overlay.py
+++ kraken/contrib/heatmap_overlay.py
@@ -2,33 +2,52 @@
 """
 Produces semi-transparent neural segmenter output overlays
 """
+import click
 
-import sys
-import torch
-from PIL import Image
-from kraken.lib import vgsl, dataset
-import torch.nn.functional as F
-from os.path import splitext
+@click.command()
+@click.option('-i', '--model', default=None, show_default=True, type=click.Path(exists=True),
+              help='Baseline detection model to use.')
+@click.argument('files', nargs=-1)
+def cli(model, files):
 
-model = vgsl.TorchVGSLModel.load_model(sys.argv[1])
-model.eval()
-batch, channels, height, width = model.input
+    import sys
+    import torch
+    from PIL import Image
+    from kraken.lib import vgsl, dataset
+    import torch.nn.functional as F
+    from os.path import splitext
+    import torchvision.transforms as tf
 
-transforms = dataset.generate_input_transforms(batch, height, width, channels, 0, valid_norm=False)
+    model = vgsl.TorchVGSLModel.load_model(model)
+    model.eval()
+    batch, channels, height, width = model.input
 
-imgs = sys.argv[2:]
-torch.set_num_threads(1)
+    transforms = dataset.generate_input_transforms(batch, height, width, channels, 0, valid_norm=False)
 
-for img in imgs:
-    print(img)
-    im = Image.open(img)
-    with torch.no_grad():
-        o = model.nn(transforms(im).unsqueeze(0))
-        o = F.interpolate(o, size=im.size[::-1])
-        o = o.squeeze().numpy()
-    heat = Image.fromarray((o[1]*255).astype('uint8'))
-    heat.save(splitext(img)[0] + '.heat.png')
-    overlay = Image.new('RGBA', im.size, (0, 130, 200, 255))
-    Image.composite(overlay, im.convert('RGBA'), heat).save(splitext(img)[0] + '.overlay.png')
-    del o
-    del im
+    torch.set_num_threads(1)
+
+    for img in files:
+        print(img)
+        im = Image.open(img)
+        res_tf = tf.Compose(transforms.transforms[:3])
+        scal_im = res_tf(im)
+
+        with torch.no_grad():
+            o, _ = model.nn(transforms(im).unsqueeze(0))
+            o = F.interpolate(o, size=scal_im.size[::-1])
+            o = o.squeeze().numpy()
+        heat = Image.fromarray((o[2]*255).astype('uint8'))
+        heat.save(splitext(img)[0] + '.heat.png')
+        overlay = Image.new('RGBA', scal_im.size, (0, 130, 200, 255))
+        bl = Image.composite(overlay, scal_im.convert('RGBA'), heat)
+        heat = Image.fromarray((o[1]*255).astype('uint8'))
+        overlay = Image.new('RGBA', scal_im.size, (230, 25, 75, 255))
+        bl = Image.composite(overlay, bl, heat)
+        heat = Image.fromarray((o[0]*255).astype('uint8'))
+        overlay = Image.new('RGBA', scal_im.size, (60, 180, 75, 255))
+        bl = Image.composite(overlay, bl, heat).save(splitext(img)[0] + '.overlay.png')
+        del o
+        del im
+
+if __name__ == '__main__':
+    cli()
