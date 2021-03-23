b8a3adacbc5282c091f7c783122d90ed14cb15a1
Benjamin Kiessling <mittagessen@l.unchti.me>
2021-03-23 16:00:58 +0100

    add cli parameters for lr schedulers

--- kraken/ketos.py
+++ kraken/ketos.py
@@ -116,8 +116,12 @@ def _validate_merging(ctx, param, value):
 @click.option('-r', '--lrate', show_default=True, default=SEGMENTATION_HYPER_PARAMS['lrate'], help='Learning rate')
 @click.option('-m', '--momentum', show_default=True, default=SEGMENTATION_HYPER_PARAMS['momentum'], help='Momentum')
 @click.option('-w', '--weight-decay', show_default=True, default=SEGMENTATION_HYPER_PARAMS['weight_decay'], help='Weight decay')
-@click.option('--schedule', show_default=True, type=click.Choice(['constant', '1cycle', 'exponential']), default=SEGMENTATION_HYPER_PARAMS['schedule'],
-              help='Set learning rate scheduler. For 1cycle/exponential, cycle/halving length is determined by the `--epoch` option.')
+@click.option('--schedule', show_default=True, type=click.Choice(['constant', '1cycle', 'exponential', 'cosine', 'step', 'reduceonplateau']), default=RECOGNITION_HYPER_PARAMS['schedule'],
+              help='Set learning rate scheduler. For 1cycle, cycle length is determined by the `--epoch` option.')
+@click.option('-g', '--gamma', show_default=True, default=RECOGNITION_HYPER_PARAMS['gamma'], help='Decay factor for exponential, step, and reduceonplateau learning rate schedules')
+@click.option('-ss', '--step-size', show_default=True, default=RECOGNITION_HYPER_PARAMS['step_size'], help='Number of validation runs between learning rate decay for exponential and step LR schedules')
+@click.option('--sched-patience', show_default=True, default=RECOGNITION_HYPER_PARAMS['rop_patience'], help='Minimal number of validation runs between LR reduction for reduceonplateau LR schedule.')
+@click.option('--cos-max', show_default=True, default=RECOGNITION_HYPER_PARAMS['cos_t_max'], help='Epoch of minimal learning rate for cosine LR scheduler.')
 @click.option('-p', '--partition', show_default=True, default=0.9, help='Ground truth data partition ratio between train/validation set')
 @click.option('-t', '--training-files', show_default=True, default=None, multiple=True,
               callback=_validate_manifests, type=click.File(mode='r', lazy=True),
@@ -154,8 +158,9 @@ def _validate_merging(ctx, param, value):
 @click.argument('ground_truth', nargs=-1, callback=_expand_gt, type=click.Path(exists=False, dir_okay=False))
 def segtrain(ctx, output, spec, line_width, load, freq, quit, epochs,
              lag, min_delta, device, optimizer, lrate, momentum, weight_decay,
-             schedule, partition, training_files, evaluation_files, threads,
-             load_hyper_parameters, force_binarization, format_type, suppress_regions,
+             schedule, gamma, step_size, sched_patience, cos_max, partition,
+             training_files, evaluation_files, threads, load_hyper_parameters,
+             force_binarization, format_type, suppress_regions,
              suppress_baselines, valid_regions, valid_baselines, merge_regions,
              merge_baselines, bounding_regions, augment, resize, ground_truth):
     """
@@ -191,7 +196,11 @@ def segtrain(ctx, output, spec, line_width, load, freq, quit, epochs,
                          'momentum': momentum,
                          'weight_decay': weight_decay,
                          'schedule': schedule,
-                         'augment': augment
+                         'augment': augment,
+                         'gamma': gamma,
+                         'step_size': step_size,
+                         'rop_patience': sched_patience,
+                         'cos_t_max': cos_max
                         })
 
     # disable automatic partition when given evaluation set explicitly
@@ -291,8 +300,12 @@ def segtrain(ctx, output, spec, line_width, load, freq, quit, epochs,
 @click.option('-r', '--lrate', show_default=True, default=RECOGNITION_HYPER_PARAMS['lrate'], help='Learning rate')
 @click.option('-m', '--momentum', show_default=True, default=RECOGNITION_HYPER_PARAMS['momentum'], help='Momentum')
 @click.option('-w', '--weight-decay', show_default=True, default=RECOGNITION_HYPER_PARAMS['weight_decay'], help='Weight decay')
-@click.option('--schedule', show_default=True, type=click.Choice(['constant', '1cycle', 'exponential']), default=RECOGNITION_HYPER_PARAMS['schedule'],
-              help='Set learning rate scheduler. For 1cycle/exponential, cycle length is determined by the `--epoch` option.')
+@click.option('--schedule', show_default=True, type=click.Choice(['constant', '1cycle', 'exponential', 'cosine', 'step', 'reduceonplateau']), default=RECOGNITION_HYPER_PARAMS['schedule'],
+              help='Set learning rate scheduler. For 1cycle, cycle length is determined by the `--epoch` option.')
+@click.option('-g', '--gamma', show_default=True, default=RECOGNITION_HYPER_PARAMS['gamma'], help='Decay factor for exponential, step, and reduceonplateau learning rate schedules')
+@click.option('-ss', '--step-size', show_default=True, default=RECOGNITION_HYPER_PARAMS['step_size'], help='Number of validation runs between learning rate decay for exponential and step LR schedules')
+@click.option('--sched-patience', show_default=True, default=RECOGNITION_HYPER_PARAMS['rop_patience'], help='Minimal number of validation runs between LR reduction for reduceonplateau LR schedule.')
+@click.option('--cos-max', show_default=True, default=RECOGNITION_HYPER_PARAMS['cos_t_max'], help='Epoch of minimal learning rate for cosine LR scheduler.')
 @click.option('-p', '--partition', show_default=True, default=0.9, help='Ground truth data partition ratio between train/validation set')
 @click.option('-u', '--normalization', show_default=True, type=click.Choice(['NFD', 'NFKD', 'NFC', 'NFKC']),
               default=RECOGNITION_HYPER_PARAMS['normalization'], help='Ground truth normalization')
@@ -339,8 +352,9 @@ def segtrain(ctx, output, spec, line_width, load, freq, quit, epochs,
 @click.argument('ground_truth', nargs=-1, callback=_expand_gt, type=click.Path(exists=False, dir_okay=False))
 def train(ctx, batch_size, pad, output, spec, append, load, freq, quit, epochs,
           lag, min_delta, device, optimizer, lrate, momentum, weight_decay,
-          schedule, partition, normalization, normalize_whitespace, codec,
-          resize, reorder, training_files, evaluation_files, preload, threads,
+          schedule, gamma, step_size, sched_patience, cos_max, partition,
+          normalization, normalize_whitespace, codec, resize, reorder,
+          training_files, evaluation_files, preload, threads,
           load_hyper_parameters, repolygonize, force_binarization, format_type,
           augment, ground_truth):
     """
@@ -369,6 +383,10 @@ def train(ctx, batch_size, pad, output, spec, append, load, freq, quit, epochs,
                          'momentum': momentum,
                          'weight_decay': weight_decay,
                          'schedule': schedule,
+                         'gamma': gamma,
+                         'step_size': step_size,
+                         'rop_patience': sched_patience,
+                         'cos_t_max': cos_max,
                          'normalization': normalization,
                          'normalize_whitespace': normalize_whitespace,
                          'augment': augment
