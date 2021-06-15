456266ba9798f471b264cf41b899bf327623b33c
Benjamin Kiessling <mittagessen@l.unchti.me>
2021-06-15 20:45:49 +0200

    baseline overlay dataset script

---
+++ kraken/contrib/baselineset_overlay.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+"""
+Produces semi-transparent neural segmenter output overlays
+"""
+import click
+
+@click.command()
+@click.argument('files', nargs=-1)
+def cli(files):
+
+    import sys
+    import torch
+    from PIL import Image
+    from kraken.lib import vgsl, dataset
+    import torch.nn.functional as F
+    from os.path import splitext
+    import torchvision.transforms as tf
+    from kraken.lib import dataset, segmentation, vgsl
+
+    batch, channels, height, width = 1, 3, 1200, 0
+    transforms = dataset.generate_input_transforms(batch, height, width, channels, 0, valid_norm=False)
+
+    torch.set_num_threads(1)
+
+    ds = dataset.BaselineSet(files, im_transforms=transforms, mode='xml')
+
+    for idx, batch in enumerate(ds):
+        img = ds.imgs[idx]
+        print(img)
+        im = Image.open(img)
+        res_tf = tf.Compose(transforms.transforms[:3])
+        scal_im = res_tf(im)
+        o = batch['target'].numpy()
+        heat = Image.fromarray((o[ds.class_mapping['baselines']['default']]*255).astype('uint8'))
+        heat.save(splitext(img)[0] + '.heat.png')
+        overlay = Image.new('RGBA', scal_im.size, (0, 130, 200, 255))
+        bl = Image.composite(overlay, scal_im.convert('RGBA'), heat)
+        heat = Image.fromarray((o[ds.class_mapping['aux']['_start_separator']]*255).astype('uint8'))
+        overlay = Image.new('RGBA', scal_im.size, (230, 25, 75, 255))
+        bl = Image.composite(overlay, bl, heat)
+        heat = Image.fromarray((o[ds.class_mapping['aux']['_end_separator']]*255).astype('uint8'))
+        overlay = Image.new('RGBA', scal_im.size, (60, 180, 75, 255))
+        bl = Image.composite(overlay, bl, heat).save(splitext(img)[0] + '.overlay.png')
+        del o
+        del im
+
+if __name__ == '__main__':
+    cli()
