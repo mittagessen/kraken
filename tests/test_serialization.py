d953d3bc9ccf8904d0660c4862916f8ce2e4883f
Benjamin Kiessling <mittagessen@l.unchti.me>
2021-10-20 15:01:06 +0200

    rename serialization tests only testing box data

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -25,7 +25,7 @@ class TestSerializations(unittest.TestCase):
             self.records = [rpred.ocr_record(**x) for x in json.load(fp)]
         self.validator = HocrValidator('standard')
 
-    def test_vertical_hocr_serialization(self):
+    def test_box_vertical_hocr_serialization(self):
         """
         Test vertical line hOCR serialization
         """
@@ -37,7 +37,7 @@ class TestSerializations(unittest.TestCase):
         report = self.validator.validate(fp, parse_strict=True)
         self.assertTrue(report.is_valid())
 
-    def test_hocr_serialization(self):
+    def test_box_hocr_serialization(self):
         """
         Test hOCR serialization
         """
@@ -49,7 +49,7 @@ class TestSerializations(unittest.TestCase):
         report = self.validator.validate(fp, parse_strict=True)
         self.assertTrue(report.is_valid())
 
-    def test_alto_serialization_validation(self):
+    def test_box_alto_serialization_validation(self):
         """
         Validates output against ALTO schema
         """
@@ -61,7 +61,7 @@ class TestSerializations(unittest.TestCase):
             alto_schema = etree.XMLSchema(etree.parse(schema_fp))
             alto_schema.assertValid(doc)
 
-    def test_abbyyxml_serialization_validation(self):
+    def test_box_abbyyxml_serialization_validation(self):
         """
         Validates output against abbyyXML schema
         """
@@ -73,7 +73,7 @@ class TestSerializations(unittest.TestCase):
             abbyy_schema = etree.XMLSchema(etree.parse(schema_fp))
             abbyy_schema.assertValid(doc)
 
-    def test_pagexml_serialization_validation(self):
+    def test_box_pagexml_serialization_validation(self):
         """
         Validates output against abbyyXML schema
         """
@@ -82,5 +82,5 @@ class TestSerializations(unittest.TestCase):
         fp.write(serialization.serialize(self.records, image_name='foo.png', template='pagexml'))
         doc = etree.fromstring(fp.getvalue().encode('utf-8'))
         with open(os.path.join(resources, 'pagecontent.xsd')) as schema_fp:
-            abbyy_schema = etree.XMLSchema(etree.parse(schema_fp))
-            abbyy_schema.assertValid(doc)
+            page_schema = etree.XMLSchema(etree.parse(schema_fp))
+            page_schema.assertValid(doc)
