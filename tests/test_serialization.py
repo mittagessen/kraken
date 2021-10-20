7c571a19a15f4dd009a19c0d241829e72a2779c4
Benjamin Kiessling <mittagessen@l.unchti.me>
2021-10-20 15:21:21 +0200

    test for duplicate IDs in serializer output

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -9,12 +9,49 @@ import os
 from lxml import etree
 from io import StringIO
 from hocr_spec import HocrValidator
+from collections import Counter
 
 from kraken import rpred
 from kraken import serialization
 
 thisfile = os.path.abspath(os.path.dirname(__file__))
 resources = os.path.abspath(os.path.join(thisfile, 'resources'))
+
+def validate_hocr(self, fp):
+    fp.seek(0)
+
+    validator = HocrValidator('standard')
+    report = validator.validate(fp, parse_strict=True)
+    self.assertTrue(report.is_valid())
+
+    doc = etree.fromstring(fp.getvalue().encode('utf-8'))
+
+    ids = [x.get('id') for x in doc.findall('.//*[@id]')]
+    counts = Counter(ids)
+    self.assertEqual(counts.most_common(1)[0][1], 1, msg='Duplicate IDs in hOCR output')
+
+def validate_page(self, fp):
+    doc = etree.fromstring(fp.getvalue().encode('utf-8'))
+
+    ids = [x.get('id') for x in doc.findall('.//*[@id]')]
+    counts = Counter(ids)
+    self.assertEqual(counts.most_common(1)[0][1], 1, msg='Duplicate IDs in PageXML output')
+
+    with open(os.path.join(resources, 'pagecontent.xsd')) as schema_fp:
+        page_schema = etree.XMLSchema(etree.parse(schema_fp))
+        page_schema.assertValid(doc)
+
+def validate_alto(self, fp):
+    doc = etree.fromstring(fp.getvalue().encode('utf-8'))
+
+    ids = [x.get('ID') for x in doc.findall('.//*[@ID]')]
+    counts = Counter(ids)
+    self.assertEqual(counts.most_common(1)[0][1], 1, msg='Duplicate IDs in ALTO output')
+
+    with open(os.path.join(resources, 'alto-4-2.xsd')) as schema_fp:
+        alto_schema = etree.XMLSchema(etree.parse(schema_fp))
+        alto_schema.assertValid(doc)
+
 
 class TestSerializations(unittest.TestCase):
     """
@@ -23,7 +60,6 @@ class TestSerializations(unittest.TestCase):
     def setUp(self):
         with open(os.path.join(resources, 'records.json'), 'r') as fp:
             self.records = [rpred.ocr_record(**x) for x in json.load(fp)]
-        self.validator = HocrValidator('standard')
 
     def test_box_vertical_hocr_serialization(self):
         """
@@ -32,10 +68,7 @@ class TestSerializations(unittest.TestCase):
         fp = StringIO()
 
         fp.write(serialization.serialize(self.records, image_name='foo.png', writing_mode='vertical-lr', template='hocr'))
-        fp.seek(0)
-
-        report = self.validator.validate(fp, parse_strict=True)
-        self.assertTrue(report.is_valid())
+        validate_hocr(self, fp)
 
     def test_box_hocr_serialization(self):
         """
@@ -44,10 +77,7 @@ class TestSerializations(unittest.TestCase):
         fp = StringIO()
 
         fp.write(serialization.serialize(self.records, image_name='foo.png', template='hocr'))
-        fp.seek(0)
-
-        report = self.validator.validate(fp, parse_strict=True)
-        self.assertTrue(report.is_valid())
+        validate_hocr(self, fp)
 
     def test_box_alto_serialization_validation(self):
         """
@@ -56,10 +86,7 @@ class TestSerializations(unittest.TestCase):
         fp = StringIO()
 
         fp.write(serialization.serialize(self.records, image_name='foo.png', template='alto'))
-        doc = etree.fromstring(fp.getvalue().encode('utf-8'))
-        with open(os.path.join(resources, 'alto-4-2.xsd')) as schema_fp:
-            alto_schema = etree.XMLSchema(etree.parse(schema_fp))
-            alto_schema.assertValid(doc)
+        validate_alto(self, fp)
 
     def test_box_abbyyxml_serialization_validation(self):
         """
@@ -80,7 +107,4 @@ class TestSerializations(unittest.TestCase):
         fp = StringIO()
 
         fp.write(serialization.serialize(self.records, image_name='foo.png', template='pagexml'))
-        doc = etree.fromstring(fp.getvalue().encode('utf-8'))
-        with open(os.path.join(resources, 'pagecontent.xsd')) as schema_fp:
-            page_schema = etree.XMLSchema(etree.parse(schema_fp))
-            page_schema.assertValid(doc)
+        validate_page(self, fp)
