Rebuild Arrow alphabet if the normalization is set. Fixes #340

kraken/lib/dataset.py
@@ -561,6 +561,30 @@ class ArrowIPCRecognitionDataset(Dataset):
             self.arrow_table = pa.concat_tables([self.arrow_table, ds_table])
         self._num_lines += num_lines
 
+    def rebuild_alphabet(self):
+        self.alphabet = Counter()
+        for index in range(len(self)):
+            text = self._apply_text_transform(
+                self.arrow_table.column('lines')[index].as_py(),
+                raise_on_empty=False
+            )
+            self.alphabet.update(text)
+
+    def _apply_text_transform(self, sample, raise_on_empty: bool = True) -> str:
+        """ Applies text transform to a sample.
+
+        Raise_on_empty raises an Exception if it finds an empty line.
+
+        """
+        text = sample['text']
+        for func in self.text_transforms:
+            text = func(text)
+        if not text:
+            logger.debug(f'Text line "{sample["text"]}" is empty after transformations')
+            if raise_on_empty:
+                raise Exception('empty text line')
+        return text
+
     def encode(self, codec: Optional[PytorchCodec] = None) -> None:
         """
         Adds a codec to the dataset.
@@ -570,9 +594,10 @@ class ArrowIPCRecognitionDataset(Dataset):
             logger.info(f'Trying to encode dataset with codec {codec}')
             for index in range(self._num_lines):
                 try:
-                    text = self.arrow_table.column('lines')[index].as_py()['text']
-                    for func in self.text_transforms:
-                        text = func(text)
+                    text = self._apply_text_transform(
+                        self.arrow_table.column('lines')[index].as_py(),
+                        raise_on_empty=False
+                    )
                     self.codec.encode(text)
                 except KrakenEncodeException as e:
                     raise e
@@ -597,12 +622,7 @@ class ArrowIPCRecognitionDataset(Dataset):
                 im = im.permute((1, 2, 0)).numpy()
                 o = self.aug(image=im)
                 im = torch.tensor(o['image'].transpose(2, 0, 1))
-            text = sample['text']
-            for func in self.text_transforms:
-                text = func(text)
-            if not text:
-                logger.debug(f'Text line "{sample["text"]}" is empty after transformations')
-                raise Exception('empty text line')
+            text = self._apply_text_transform(sample)
         except Exception:
             idx = np.random.randint(0, len(self))
             logger.debug(traceback.format_exc())