317fe69ad5ce519faebe0567ecbe10e0c088f609
Benjamin Kiessling <mittagessen@l.unchti.me>
2022-05-11 03:27:54 +0200

    deprecate obsolete pyrnn.gz and prornn serialization formats

--- tests/test_models.py
+++ tests/test_models.py
@@ -33,33 +33,3 @@ class TestModels(unittest.TestCase):
         """
         with raises(KrakenInvalidModelException):
             models.load_any(self.temp.name)
-
-    def test_load_clstm(self):
-        """
-        Tests loading of valid clstm files.
-        """
-        rnn = models.load_any(resources / 'toy.clstm')
-        self.assertIsInstance(rnn, models.TorchSeqRecognizer)
-
-    def test_load_pyrnn_no_seqrecognizer(self):
-        """
-        Test correct handling of non-SeqRecognizer pickles.
-        """
-        pickle.dump(u'Iámnõtãrécðçnízer', self.temp)
-        self.temp.close()
-        with raises(KrakenInvalidModelException):
-            models.load_any(self.temp.name)
-
-    def test_load_any_pyrnn_py3(self):
-        """
-        Test load_any doesn't load pickled models on python 3
-        """
-        with raises(KrakenInvalidModelException):
-            rnn = models.load_any(resources / 'model.pyrnn.gz')
-
-    def test_load_any_proto(self):
-        """
-        Test load_any loads protobuf models.
-        """
-        rnn = models.load_any(resources / 'model.pronn')
-        self.assertIsInstance(rnn, kraken.lib.models.TorchSeqRecognizer)
