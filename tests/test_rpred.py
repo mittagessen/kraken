ba01ccec833aa7c565422f25202ad9b63694223e
Benjamin Kiessling <mittagessen@l.unchti.me>
2022-09-16 11:45:57 +0200

    working bbox record tests

--- tests/test_rpred.py
+++ tests/test_rpred.py
@@ -32,15 +32,36 @@ class TestBBoxRecords(unittest.TestCase):
         """
         Make sure that the cuts of a record are converted to absolute coordinates.
         """
-        record = BBoxOCRRecord(*self.ltr_record)
-        self.assertEqual(record[2:8][1], ((1508, 119), (1674, 119), (1674, 256), (1508, 256)))
+        record = BBoxOCRRecord(**self.ltr_record)
+        self.assertEqual(record.cuts, [[[1437, 119], [1437, 256], [1449, 256], [1449, 119]],
+                                       [[1484, 119], [1484, 256], [1496, 256], [1496, 119]],
+                                       [[1508, 119], [1508, 256], [1520, 256], [1520, 119]],
+                                       [[1568, 119], [1568, 256], [1568, 256], [1568, 119]],
+                                       [[1603, 119], [1603, 256], [1603, 256], [1603, 119]],
+                                       [[1615, 119], [1615, 256], [1627, 256], [1627, 119]],
+                                       [[1639, 119], [1639, 256], [1639, 256], [1639, 119]],
+                                       [[1663, 119], [1663, 256], [1674, 256], [1674, 119]],
+                                       [[1698, 119], [1698, 256], [1698, 256], [1698, 119]],
+                                       [[1722, 119], [1722, 256], [1734, 256], [1734, 119]],
+                                       [[1746, 119], [1746, 256], [1758, 256], [1758, 119]],
+                                       [[1793, 119], [1793, 256], [1805, 256], [1805, 119]],
+                                       [[1817, 119], [1817, 256], [1829, 256], [1829, 119]],
+                                       [[1853, 119], [1853, 256], [1853, 256], [1853, 119]],
+                                       [[1876, 119], [1876, 256], [1888, 256], [1888, 119]],
+                                       [[1924, 119], [1924, 256], [1936, 256], [1936, 119]],
+                                       [[1959, 119], [1959, 256], [1971, 256], [1971, 119]],
+                                       [[2007, 119], [2007, 256], [2019, 256], [2019, 119]],
+                                       [[2054, 119], [2054, 256], [2054, 256], [2054, 119]],
+                                       [[2078, 119], [2078, 256], [2090, 256], [2090, 119]],
+                                       [[2149, 119], [2149, 256], [2149, 256], [2149, 119]],
+                                       [[2161, 119], [2161, 256], [2173, 256], [2173, 119]]])
 
     def test_bbox_record_redisplay(self):
         """
         Test that a display order record remains in display order when
         requesting a DO record.
         """
-        record = BBoxOCRRecord(*self.ltr_record, display_order=True)
+        record = BBoxOCRRecord(**self.ltr_record, display_order=True)
         self.assertEqual(record, record.display_order())
 
     def test_bbox_record_relogical(self):
@@ -48,44 +69,48 @@ class TestBBoxRecords(unittest.TestCase):
         Test that a logical order record remains in logical order when
         requesting a LO record.
         """
-        record = BBoxOCRRecord(*self.ltr_record, display_order=False)
+        record = BBoxOCRRecord(**self.ltr_record, display_order=False)
         self.assertEqual(record, record.logical_order())
 
     def test_bbox_record_display(self):
         """
         test display order conversion of record.
         """
-        record = BBoxOCRRecord(*self.ltr_record, display_order=False)
+        record = BBoxOCRRecord(**self.ltr_record, display_order=False)
         re_record = record.display_order()
         self.assertEqual(re_record.prediction, 'في معجزاته عليه السلام')
         self.assertEqual(re_record[:][1], ((1437, 119), (2173, 119), (2173, 256), (1437, 256)))
-        self.assertAlmostEqual(re_record[2:8][2], 0.9554762)
+        self.assertAlmostEqual(re_record[2:8][2], 0.9554762, places=4)
 
     def test_bbox_record_logical(self):
         """
         Test logical order conversion of record.
         """
-        record = BBoxOCRRecord(*self.ltr_record, display_order=True)
+        record = BBoxOCRRecord(**self.ltr_record, display_order=True)
         re_record = record.logical_order()
         self.assertEqual(re_record.prediction, 'في معجزاته عليه السلام')
         self.assertEqual(re_record[:][1], ((1437, 119), (2173, 119), (2173, 256), (1437, 256)))
-        self.assertAlmostEqual(re_record[2:8][2], 0.9554762)
+        self.assertAlmostEqual(re_record[2:8][2], 0.9554762, places=4)
 
     def test_bbox_record_slicing(self):
         """
         Tests simple slicing/aggregation of elements in record.
         """
-        pass 
+        record = BBoxOCRRecord(**self.ltr_record, display_order=True)
+        pred, cut, conf = record[1:8]
+        self.assertEqual(pred, 'السلا ه')
+        self.assertEqual(cut, ((1484, 119), (1674, 119), (1674, 256), (1484, 256)))
+        self.assertAlmostEqual(conf, 0.9259478, places=4)
 
     def test_bbox_record_slicing(self):
         """
         Tests complex slicing/aggregation of elements in record.
         """
-        record = BBoxOCRRecord(*self.ltr_record, display_order=True)
+        record = BBoxOCRRecord(**self.ltr_record, display_order=True)
         pred, cut, conf = record[1:5:2]
         self.assertEqual(pred, 'اس')
         self.assertEqual(cut, ((1484, 119), (1568, 119), (1568, 256), (1484, 256)))
-        self.assertAlmostEqual(conf, 0.74411)
+        self.assertAlmostEqual(conf, 0.74411, places=4)
 
 class TestBaselineRecords(unittest.TestCase):
     """
