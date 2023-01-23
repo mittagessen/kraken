7f2cbe6db19cacd53f14e0f1d1362a86dc75c26e
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-01-23 13:08:43 +0100

    Add padding options to set_seg_options.py

--- kraken/contrib/set_seg_options.py
+++ kraken/contrib/set_seg_options.py
@@ -9,8 +9,10 @@ import shutil
 @click.command()
 @click.option('-b', '--bounding-region', multiple=True, help='Sets region identifiers which bound line bounding polygons')
 @click.option('--topline/--baseline', default=False, help='Sets model line type to baseline or topline')
+@click.option('--pad', show_default=True, type=(int, int), default=(0, 0),
+              help='Padding (left/right, top/bottom) around the page image')
 @click.argument('model', nargs=1, type=click.Path(exists=True))
-def cli(bounding_region, topline, model):
+def cli(bounding_region, topline, pad, model):
     """
     A script setting the metadata of segmentation models.
     """
@@ -41,6 +43,11 @@ def cli(bounding_region, topline, model):
     print(f'Model is {"topline" if "topline" in net.user_metadata and net.user_metadata["topline"] else "baseline"}')
     print(f'Setting to  {"topline" if topline else "baseline"}')
     net.user_metadata['topline'] = topline
+
+    print(f"Model has padding {net.user_metadata['hyper_params']['padding'] if 'padding' in net.user_metadata['hyper_params'] else (0, 0)}")
+    print(f'Setting to {pad}')
+    net.user_metadata['hyper_params']['padding'] = pad
+
     shutil.copy(model, f'{model}.bak')
     net.save_model(model)
 
