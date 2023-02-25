545910401d8223174f2f451af5823ac688cf16ad
Colin Brisson <colibrisson@gmail.com>
2023-02-25 08:20:11 +0000

    set default precision to 32-bit

--- kraken/ketos/segmentation.py
+++ kraken/ketos/segmentation.py
@@ -99,9 +99,9 @@ def _validate_merging(ctx, param, value):
 @click.option('-d', '--device', show_default=True, default='cpu', help='Select device to use (cpu, cuda:0, cuda:1, ...)')
 @click.option('--precision',
               show_default=True,
-              default='16',
+              default='32',
               type=click.Choice(['64', '32', 'bf16', '16']),
-              help='Numerical precision to use for training. Default is 16-bit mixed precision.')
+              help='Numerical precision to use for training. Default is 32-bit single-point precision.')
 @click.option('--optimizer',
               show_default=True,
               default=SEGMENTATION_HYPER_PARAMS['optimizer'],
