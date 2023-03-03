bb4999ee7d7241d3f8daa86a6531ce8c47940a60
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-03-03 12:45:38 +0100

    working training code for RO

---
+++ kraken/lib/ro/layers.py
@@ -0,0 +1,27 @@
+"""
+Layers for VGSL models
+"""
+from torch import nn
+
+# all tensors are ordered NCHW, the "feature" dimension is C, so the output of
+# an LSTM will be put into C same as the filters of a CNN.
+
+__all__ = ['MLP']
+
+
+class MLP(nn.Module):
+    """
+    A simple 2 layer MLP for reading order determination.
+    """
+    def __init__(self, feature_size: int, hidden_size: int):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(feature_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return self.sigmoid(x)
