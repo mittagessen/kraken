3c4b9d2c2b4bd0cf766d9222146e24800f7f4d9e
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-03-06 23:39:13 +0100

    use original implementation hidden size

--- kraken/lib/ro/model.py
+++ kraken/lib/ro/model.py
@@ -128,7 +128,7 @@ class ROModel(pl.LightningModule):
         self.best_metric = torch.inf
 
         logger.info(f'Creating new RO model')
-        self.ro_net = torch.jit.script(MLP(train_set.get_feature_dim(), 128))
+        self.ro_net = torch.jit.script(MLP(train_set.get_feature_dim(), train_set.get_feature_dim() * 2))
 
         if 'file_system' in torch.multiprocessing.get_all_sharing_strategies():
             logger.debug('Setting multiprocessing tensor sharing strategy to file_system')
