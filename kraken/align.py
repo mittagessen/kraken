73a6adb6c74184dc843a843592b8c68cfa64135f
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-03-15 15:12:36 +0100

    openfst-less forced alignment script

--- kraken/align.py
+++ kraken/align.py
@@ -22,13 +22,15 @@ A character alignment module using a network output lattice and ground truth to
 accuractely determine grapheme locations in input data. Requires OpenFST and
 either the official python bindings or pywrapfst.
 """
+import torch
 import logging
 import numpy as np
 
 from PIL import Image
 from bidi.algorithm import get_display
 
-from typing import List, Dict, Any
+from dataclasses import dataclass
+from typing import List, Dict, Any, Optional, Literal
 
 from kraken import rpred
 from kraken.lib.codec import PytorchCodec
@@ -38,147 +40,8 @@ from kraken.lib.segmentation import compute_polygon_section
 
 logger = logging.getLogger('kraken')
 
-try:
-    import pywrapfst as fst
 
-    _get_fst = lambda: fst.VectorFst() # NOQA
-    _get_best_weight = lambda x: fst.Weight.one(x.weight_type()) # NOQA
-except ImportError:
-    logger.info('pywrapfst not available. Falling back to openfst_python.')
-    try:
-        import openfst_python as fst
-        _get_fst = lambda: fst.Fst() # NOQA
-        _get_best_weight = lambda x: 0 # NOQA
-    except ImportError:
-        logger.error('Neither pywrapfst nor openfst_python bindings available.')
-        raise
-
-
-def _get_arc(input_label, output_label, weight, state):
-    return fst.Arc(input_label, output_label, weight, state)
-
-
-def _compose(first_graph, second_graph):
-    return fst.compose(first_graph, second_graph)
-
-
-def _shortest_path(graph):
-    return fst.shortestpath(graph)
-
-
-def fst_from_lattice(outputs: np.ndarray):
-    """
-    Generates an FST from the network output tensor.
-
-    Arguments:
-        outputs: Output tensor of shape (batch, labels, length).
-
-    Returns:
-        An OpenFST FST object.
-    """
-    f = _get_fst()
-    state = f.add_state()
-    f.set_start(state)
-    for idx, frame in enumerate(np.transpose(outputs[0], [1, 0])):
-        next_state = f.add_state()
-        for label, conf in enumerate(frame):
-            f.add_arc(state, _get_arc(idx, label+1, -np.log(conf), next_state))
-        state = next_state
-
-    f.set_final(state)
-
-    if not f.verify():
-        raise KrakenInputException('Invalid FST')
-
-    return f
-
-
-def fst_from_text(line: str, codec: PytorchCodec):
-    """
-    Creates an FST of a text line in label space.
-
-    In order to match the Kraken output, we take into account the fact that
-    Kraken will output a large number of frames and therefore each character
-    of our line must be able to be detected between several non-characters "#".
-
-    In the same way, Kraken can predict several times the same character on
-    neighboring frames, if this is the case it doesn't mean that there is
-    several times the same letter but only that it appears on several frames.
-
-    To handle this particular case we use Ɛ:Ɛ transitions between each character
-    of our line and we add a Ɛ:mychar loop to eliminate identical neighboring
-    characters.
-
-    To detect the word "loop", we'll have something like:
-
-       Ɛ:Ɛ    l:l    Ɛ:Ɛ    o:o    Ɛ:Ɛ    o:o    Ɛ:Ɛ    p:p
-    [0]───>(1)───>(2)───>(3)───>(4)───>(5)───>(6)───>(7)───>((8))
-     ↺      ↺      ↺      ↺      ↺      ↺      ↺      ↺       ↺
-    #:Ɛ    l:Ɛ    #:Ɛ    o:Ɛ    #:Ɛ    o:Ɛ    #:Ɛ    p:Ɛ     #:Ɛ
-    """
-    f = _get_fst()
-    one = _get_best_weight(f)
-    state = f.add_state()
-    f.set_start(state)
-
-    codec.strict = True
-
-    def _detect_char(graph, one, start_state, char, codec):
-        # Empty char loop
-        graph.add_arc(start_state, _get_arc(1, 0, one, start_state))
-        epsilon_state = graph.add_state()
-        # Epsilon transition
-        graph.add_arc(start_state, _get_arc(0, 0, one, epsilon_state))
-        # Adding one to expand neural network codec with epsilon at pos 0
-        # We will end with the empty char # at pos 1 (see: kraken.lib.codec.py)
-        try:
-            encoded_char = codec.encode(char)[0] + 1
-        except KrakenEncodeException:
-            encoded_char = 0
-            logger.warning(f'Error while encoding code point {char}. Skipping.')
-        # Espilon loop
-        graph.add_arc(epsilon_state, _get_arc(encoded_char, 0, one, epsilon_state))
-        end_state = graph.add_state()
-        # Char transition
-        graph.add_arc(epsilon_state, _get_arc(encoded_char, encoded_char, one, end_state))
-        return end_state
-
-    for char in line:
-        state = _detect_char(f, one, state, char, codec)
-
-    f.add_arc(state, _get_arc(1, 0, one, state))
-    f.set_final(state)
-
-    if not f.verify():
-        raise KrakenInputException('Invalid FST')
-
-    return f
-
-
-def _generate_line_record(graph, final_step):
-    next_state = graph.start()
-    labels = []
-    activations = [0]
-
-    while next_state:
-        state = next_state
-        next_state = None
-        for arc in graph.arcs(state):
-            next_state = arc.nextstate
-            olabel = arc.olabel - 1
-            if olabel == -1:
-                continue
-            labels.append(olabel)
-            activations.append(arc.ilabel)
-    activations.append(final_step)
-    # spread activations to halfway point between activations
-    bounds = []
-    for prev_ac, cur_ac, next_ac in zip(activations[:-2], activations[1:-1], activations[2:]):
-        bounds.append((prev_ac + (cur_ac-prev_ac)//2, cur_ac + (next_ac-cur_ac)//2))
-    return zip(bounds, labels)
-
-
-def forced_align(doc: Dict[str, Any], model: TorchSeqRecognizer) -> List[rpred.ocr_record]:
+def forced_align(doc: Dict[str, Any], model: TorchSeqRecognizer, base_dir: Optional[Literal['L', 'R']] = None) -> List[rpred.ocr_record]:
     """
     Performs a forced character alignment of text with recognition model
     output activations.
@@ -192,32 +55,138 @@ def forced_align(doc: Dict[str, Any], model: TorchSeqRecognizer) -> List[rpred.o
     """
     im = Image.open(doc['image'])
     predictor = rpred.rpred(model, im, doc)
-    records = []
-    for idx, line in enumerate(doc['lines']):
-        bidi_text = get_display(line['text'])
-        gt_fst = fst_from_text(bidi_text, model.codec)
-        next(predictor)
-        model.codec.strict = False
-        if model.outputs.shape[2] < 2*len(model.codec.encode(bidi_text)):
-            logger.warning(f'Could not align line {idx}. Output sequence length {model.outputs.shape[2]} < '
-                           f'{2*len(model.codec.encode(bidi_text))} (length of "{line["text"]}" after encoding).')
-            records.append(rpred.ocr_record('', [], [], line))
-            model.codec.strict = True
-            continue
-        model.codec.strict = True
+    if 'type' in predictor.bounds and predictor.bounds['type'] == 'baselines':
+        rec_class = rpred.BaselineOCRRecord
 
-        lat_fst = fst_from_lattice(model.outputs)
-        composed_graph = _compose(lat_fst, gt_fst)
-        short_graph = _shortest_path(composed_graph)
+    records = []
+
+    # enable training mode in last layer to get log_softmax output
+    model.nn.nn[-1].training = True
+
+    for idx, line in enumerate(doc['lines']):
+        # convert text to display order
+        do_text = get_display(line['text'], base_dir=base_dir)
+        # encode into labels, ignoring unencodable sequences
+        labels = model.codec.encode(do_text).long()
+        next(predictor)
+        if model.outputs.shape[2] < 2*len(labels):
+            logger.warning(f'Could not align line {idx}. Output sequence length {model.outputs.shape[2]} < '
+                           f'{2*len(labels)} (length of "{line["text"]}" after encoding).')
+            records.append(rpred.BaselineOCRRecord('', [], [], line))
+            continue
+        emission = torch.tensor(model.outputs).squeeze().T
+        trellis = get_trellis(emission, labels)
+        path = backtrack(trellis, emission, labels)
+        path = merge_repeats(path, do_text)
         pred = []
         pos = []
         conf = []
-        for act, label in _generate_line_record(short_graph, model.outputs.shape[2]-1):
-            pos.append(compute_polygon_section(line['baseline'],
-                                               line['boundary'],
-                                               predictor._scale_val(act[0], 0, predictor.box.size[0]),
-                                               predictor._scale_val(act[1], 0, predictor.box.size[0])))
-            conf.append(1.0)
-            pred.append(model.codec.decode([(label, 0, 0, 0)])[0][0])
-        records.append(rpred.bidi_record(rpred.ocr_record(pred, pos, conf, line)))
+        for seg in path:
+            pred.append(seg.label)
+            pos.append((predictor._scale_val(seg.start, 0, predictor.box.size[0]),
+                        predictor._scale_val(seg.end, 0, predictor.box.size[0])))
+            conf.append(seg.score)
+        records.append(rpred.BaselineOCRRecord(pred, pos, conf, line, display_order=True))
     return records
+
+
+@dataclass
+class Point:
+    token_index: int
+    time_index: int
+    score: float
+
+
+# Merge the labels
+@dataclass
+class Segment:
+    label: str
+    start: int
+    end: int
+    score: float
+
+    def __repr__(self):
+        return f"{self.label}\t({self.score:4.2f}): [{self.start:5d}, {self.end:5d})"
+
+    @property
+    def length(self):
+        return self.end - self.start
+
+
+def get_trellis(emission, tokens):
+    # width x labels in log domain
+    num_frame = emission.size(0)
+    num_tokens = len(tokens)
+
+    # Trellis has extra dimensions for both time axis and tokens.
+    # The extra dim for tokens represents <SoS> (start-of-sentence)
+    # The extra dim for time axis is for simplification of the code.
+    trellis = torch.empty((num_frame + 1, num_tokens + 1))
+    trellis[0, 0] = 0
+    trellis[1:, 0] = torch.cumsum(emission[:, 0], 0)
+    trellis[0, -num_tokens:] = -float("inf")
+    trellis[-num_tokens:, 0] = float("inf")
+
+    for t in range(num_frame):
+        trellis[t + 1, 1:] = torch.maximum(
+            # Score for staying at the same token
+            trellis[t, 1:] + emission[t, 0],
+            # Score for changing to the next token
+            trellis[t, :-1] + emission[t, tokens],
+        )
+    return trellis
+
+
+def backtrack(trellis, emission, tokens):
+    # Note:
+    # j and t are indices for trellis, which has extra dimensions
+    # for time and tokens at the beginning.
+    # When referring to time frame index `T` in trellis,
+    # the corresponding index in emission is `T-1`.
+    # Similarly, when referring to token index `J` in trellis,
+    # the corresponding index in transcript is `J-1`.
+    j = trellis.size(1) - 1
+    t_start = torch.argmax(trellis[:, j]).item()
+
+    path = []
+    for t in range(t_start, 0, -1):
+        # 1. Figure out if the current position was stay or change
+        # Note (again):
+        # `emission[J-1]` is the emission at time frame `J` of trellis dimension.
+        # Score for token staying the same from time frame J-1 to T.
+        stayed = trellis[t - 1, j] + emission[t - 1, 0]
+        # Score for token changing from C-1 at T-1 to J at T.
+        changed = trellis[t - 1, j - 1] + emission[t - 1, tokens[j - 1]]
+
+        # 2. Store the path with frame-wise probability.
+        prob = emission[t - 1, tokens[j - 1] if changed > stayed else 0].exp().item()
+        # Return token index and time index in non-trellis coordinate.
+        path.append(Point(j - 1, t - 1, prob))
+
+        # 3. Update the token
+        if changed > stayed:
+            j -= 1
+            if j == 0:
+                break
+    else:
+        raise ValueError("Failed to align")
+    return path[::-1]
+
+
+def merge_repeats(path, ground_truth):
+    i1, i2 = 0, 0
+    segments = []
+    while i1 < len(path):
+        while i2 < len(path) and path[i1].token_index == path[i2].token_index:
+            i2 += 1
+        score = sum(path[k].score for k in range(i1, i2)) / (i2 - i1)
+        segments.append(
+            Segment(
+                ground_truth[path[i1].token_index],
+                path[i1].time_index,
+                path[i2 - 1].time_index + 1,
+                score,
+            )
+        )
+        i1 = i2
+    return segments
