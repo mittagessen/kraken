7d2ca269cefde9f03d40fe87ced7320e6d6a432a
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-03-28 16:32:34 +0200

    make extract_lines work with binary datasets

--- kraken/contrib/extract_lines.py
+++ kraken/contrib/extract_lines.py
@@ -4,7 +4,7 @@ import click
 
 
 @click.command()
-@click.option('-f', '--format-type', type=click.Choice(['xml', 'alto', 'page']), default='xml',
+@click.option('-f', '--format-type', type=click.Choice(['xml', 'alto', 'page', 'binary']), default='xml',
               help='Sets the input document format. In ALTO and PageXML mode all '
               'data is extracted from xml files containing both baselines, polygons, and a '
               'link to source images.')
@@ -34,18 +34,35 @@ def cli(format_type, model, repolygonize, files):
     from os.path import splitext
     from kraken import blla
     from kraken.lib import segmentation, vgsl, xml
+    import io
+    import json
+    import pyarrow as pa
 
     if model is None:
         for doc in files:
             click.echo(f'Processing {doc} ', nl=False)
-            data = xml.preparse_xml_data([doc], format_type, repolygonize=repolygonize)
-            if len(data) > 0:
-                bounds = {'type': 'baselines', 'lines': [{'boundary': t['boundary'], 'baseline': t['baseline'], 'text': t['text']} for t in data]}
-                for idx, (im, box) in enumerate(segmentation.extract_polygons(Image.open(data[0]['image']), bounds)):
-                    click.echo('.', nl=False)
-                    im.save('{}.{}.jpg'.format(splitext(data[0]['image'])[0], idx))
-                    with open('{}.{}.gt.txt'.format(splitext(data[0]['image'])[0], idx), 'w') as fp:
-                        fp.write(box['text'])
+            if format_type != 'binary':
+                data = xml.preparse_xml_data([doc], format_type, repolygonize=repolygonize)
+                if len(data) > 0:
+                    bounds = {'type': 'baselines', 'lines': [{'boundary': t['boundary'], 'baseline': t['baseline'], 'text': t['text']} for t in data]}
+                    for idx, (im, box) in enumerate(segmentation.extract_polygons(Image.open(data[0]['image']), bounds)):
+                        click.echo('.', nl=False)
+                        im.save('{}.{}.jpg'.format(splitext(data[0]['image'])[0], idx))
+                        with open('{}.{}.gt.txt'.format(splitext(data[0]['image'])[0], idx), 'w') as fp:
+                            fp.write(box['text'])
+            else:
+                with pa.memory_map(doc, 'rb') as source:
+                    ds_table = pa.ipc.open_file(source).read_all()
+                    raw_metadata = ds_table.schema.metadata
+                    if not raw_metadata or b'lines' not in raw_metadata:
+                        raise ValueError(f'{file} does not contain a valid metadata record.')
+                    metadata = json.loads(raw_metadata[b'lines'])
+                    for idx in range(metadata['counts']['all']):
+                        sample = ds_table.column('lines')[idx].as_py()
+                        im = Image.open(io.BytesIO(sample['im']))
+                        im.save('{}.{}.jpg'.format(splitext(doc)[0], idx))
+                        with open('{}.{}.gt.txt'.format(splitext(doc)[0], idx), 'w') as fp:
+                            fp.write(sample['text'])
     else:
         net = vgsl.TorchVGSLModel.load_model(model)
         for doc in files:
