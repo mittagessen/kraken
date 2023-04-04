15820b4aab86d8aeec32b4af89dd8d0b0795f9f4
Benjamin Kiessling <mittagessen@l.unchti.me>
2023-04-04 13:06:29 +0200

    coreml serialization of RO model

--- kraken/lib/ro/layers.py
+++ kraken/lib/ro/layers.py
@@ -23,3 +23,36 @@ class MLP(nn.Module):
         x = self.fc1(x)
         x = self.relu(x)
         return self.fc2(x)
+
+    def deserialize(self, name: str, spec) -> None:
+        """
+        Sets the weights of an initialized module from a CoreML protobuf spec.
+        """
+        # extract 1st linear projection parameters
+        lin = [x for x in spec.neuralNetwork.layers if x.name == '{}_mlp_lin_1'.format(name)][0].innerProduct
+        weights = torch.Tensor(lin.weights.floatValue).resize_as_(self.fc1.weight.data)
+        bias = torch.Tensor(lin.bias.floatValue)
+        self.fc1.weight = torch.nn.Parameter(weights)
+        self.fc1.bias = torch.nn.Parameter(bias)
+        # extract 2nd linear projection parameters
+        lin = [x for x in spec.neuralNetwork.layers if x.name == '{}_mlp_lin_2'.format(name)][0].innerProduct
+        weights = torch.Tensor(lin.weights.floatValue).resize_as_(self.fc2.weight.data)
+        bias = torch.Tensor(lin.bias.floatValue)
+        self.fc2.weight = torch.nn.Parameter(weights)
+        self.fc2.bias = torch.nn.Parameter(bias)
+
+    def serialize(self, name: str, input: str, builder):
+        """
+        Serializes the module using a NeuralNetworkBuilder.
+        """
+        builder.add_inner_product(f'{name}_mlp_lin_1', self.fc1.weight.data.numpy(),
+                                  self.fc1.bias.data.numpy(),
+                                  self.feature_size, self.hidden_size,
+                                  has_bias=True, input_name=input, output_name=f'{name}_mlp_lin_1')
+        builder.add_activation(f'{name}_mlp_lin_1_relu', 'RELU', f'{name}_mlp_lin_1', f'{name}_mlp_lin_1_relu')
+        builder.add_inner_product(f'{name}_mlp_lin_1', self.fc2.weight.data.numpy(),
+                                  self.fc2.bias.data.numpy(),
+                                  self.hidden_size, 1,
+                                  has_bias=True, input_name=f'{name}_mlp_lin_1_relu', output_name=f'{name}_mlp_lin_2')
+        return name
+
