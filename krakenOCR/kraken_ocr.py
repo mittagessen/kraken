80e112faefae65e664871b95ee2e181f210376aa
Shreejan-git <hustlingshreejan@gmail.com>
2023-06-01 18:02:38 +0545

    code refactored

--- krakenOCR/kraken_ocr.py
+++ krakenOCR/kraken_ocr.py
@@ -7,28 +7,17 @@ from kraken.lib import vgsl
 class KrakenOCR:
     def __init__(self):
         self.blla = blla
-        self.model = vgsl.TorchVGSLModel.load_model('kraken/blla.mlmodel')
+        self.model = vgsl.TorchVGSLModel.load_model("blla.mlmodel")
 
-    def ocr(self, im, preprocessing=False):
-        baseline_seg = self.blla.segment(im=im, model=self.model, device='cpu')  # Baseline segmenter
+    def ocr(self, input_image):
+        preprocessed_image = nlbin(input_image)
+
+        baseline_seg = self.blla.segment(im=preprocessed_image, model=self.model, device='cpu')  # Baseline segmenter
         return baseline_seg['lines']
-
-    def img_preprocessing(self, im, inverse=False):
-        """
-        im: input image
-        inverse: Default is False. If set to True, it will inverse the binary image to foreground = white and
-        background = black.
-        """
-
-        binarized_image = nlbin(im)
-        if inverse:
-            return ImageOps.invert(binarized_image)
-
-        return binarized_image
 
 
 if __name__ == "__main__":
-    image_path = "/home/dell/Documents/handwritten_images/testingimages/d2.jpeg"
+    image_path = '/home/dell/Documents/handwritten_images/testingimages/d1.jpeg'
     image = Image.open(image_path)
     im = ImageOps.exif_transpose(image)
     k = KrakenOCR()
