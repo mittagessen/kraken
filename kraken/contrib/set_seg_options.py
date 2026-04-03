d83ceddc1f6c19a630b0a68eda64654cf11cf83c
Benjamin Kiessling <mittagessen@l.unchti.me>
2026-04-03 21:25:21 +0200

    update segmentation model metadata flag setting script

--- kraken/contrib/set_seg_options.py
+++ kraken/contrib/set_seg_options.py
@@ -14,82 +14,95 @@ import click
               flag_value='topline',
               show_default=True)
 @click.option('--centerline', 'topline', flag_value='centerline')
-@click.option('--baseline', 'topline', flag_value='topline')
-@click.option('--pad', show_default=True, type=(int, int), default=(0, 0),
+@click.option('--baseline', 'topline', flag_value='baseline')
+@click.option('--pad', show_default=True, type=(int, int), default=None,
               help='Padding (left/right, top/bottom) around the page image')
 @click.option('--output-identifiers', type=click.Path(exists=True), help='Path '
               'to a json file containing a dict updating the string identifiers '
               'of line/region classes.')
+@click.option('-f', '--format', 'output_format', type=click.Choice(['safetensors', 'coreml']),
+              default='safetensors', show_default=True, help='Output model format')
 @click.argument('model', nargs=1, type=click.Path(exists=True))
-def cli(bounding_region, topline, pad, output_identifiers, model):
+def cli(bounding_region, topline, pad, output_identifiers, output_format, model):
     """
     A script setting the metadata of segmentation models.
     """
     import json
-    from kraken.lib import vgsl
+    from pathlib import Path
+    from kraken.models import load_models
+    from kraken.models.writers import write_safetensors, write_coreml
 
-    net = vgsl.TorchVGSLModel.load_model(model)
-    if net.model_type != 'segmentation':
-        print('Model is not a segmentation model.')
+    all_models = load_models(model)
+    seg_models = [m for m in all_models if 'segmentation' in m.model_type]
+
+    if not seg_models:
+        print('No segmentation model found in file.')
         return
 
-    print('detectable line types:')
-    for k, v in net.user_metadata['class_mapping']['baselines'].items():
-        print(f'  {k}\t{v}')
-    print('detectable region types:')
-    for k, v in net.user_metadata['class_mapping']['regions'].items():
-        print(f'  {k}\t{v}')
+    for net in seg_models:
+        print('detectable line types:')
+        for k, v in net.user_metadata['class_mapping']['baselines'].items():
+            print(f'  {k}\t{v}')
+        print('detectable region types:')
+        for k, v in net.user_metadata['class_mapping']['regions'].items():
+            print(f'  {k}\t{v}')
 
-    if output_identifiers:
-        with open(output_identifiers, 'r') as fp:
-            new_cls_map = json.load(fp)
-        print('-> Updating class maps')
-        if 'baselines' in new_cls_map:
-            print('new baseline identifiers:')
-            old_cls = {v: k for k, v in net.user_metadata['class_mapping']['baselines'].items()}
-            new_cls = {v: k for k, v in new_cls_map['baselines'].items()}
-            old_cls.update(new_cls)
-            net.user_metadata['class_mapping']['baselines'] = {v: k for k, v in old_cls.items()}
-            for k, v in net.user_metadata['class_mapping']['baselines'].items():
-                print(f'  {k}\t{v}')
-        if 'regions' in new_cls_map:
-            print('new region identifiers:')
-            old_cls = {v: k for k, v in net.user_metadata['class_mapping']['regions'].items()}
-            new_cls = {v: k for k, v in new_cls_map['regions'].items()}
-            old_cls.update(new_cls)
-            net.user_metadata['class_mapping']['regions'] = {v: k for k, v in old_cls.items()}
-            for k, v in net.user_metadata['class_mapping']['regions'].items():
-                print(f'  {k}\t{v}')
+        if output_identifiers:
+            with open(output_identifiers, 'r') as fp:
+                new_cls_map = json.load(fp)
+            print('-> Updating class maps')
+            if 'baselines' in new_cls_map:
+                print('new baseline identifiers:')
+                old_cls = {v: k for k, v in net.user_metadata['class_mapping']['baselines'].items()}
+                new_cls = {v: k for k, v in new_cls_map['baselines'].items()}
+                old_cls.update(new_cls)
+                net.user_metadata['class_mapping']['baselines'] = {v: k for k, v in old_cls.items()}
+                for k, v in net.user_metadata['class_mapping']['baselines'].items():
+                    print(f'  {k}\t{v}')
+            if 'regions' in new_cls_map:
+                print('new region identifiers:')
+                old_cls = {v: k for k, v in net.user_metadata['class_mapping']['regions'].items()}
+                new_cls = {v: k for k, v in new_cls_map['regions'].items()}
+                old_cls.update(new_cls)
+                net.user_metadata['class_mapping']['regions'] = {v: k for k, v in old_cls.items()}
+                for k, v in net.user_metadata['class_mapping']['regions'].items():
+                    print(f'  {k}\t{v}')
 
-    print(f'existing bounding regions: {net.user_metadata["bounding_regions"]}')
+        print(f'existing bounding regions: {net.user_metadata["bounding_regions"]}')
 
-    if bounding_region:
-        br = set(net.user_metadata["bounding_regions"])
-        br_new = set(bounding_region)
+        if bounding_region:
+            br = set(net.user_metadata["bounding_regions"])
+            br_new = set(bounding_region)
 
-        print(f'-> removing: {br.difference(br_new)}')
-        print(f'-> adding: {br_new.difference(br)}')
-        net.user_metadata["bounding_regions"] = bounding_region
+            print(f'-> removing: {br.difference(br_new)}')
+            print(f'-> adding: {br_new.difference(br)}')
+            net.user_metadata["bounding_regions"] = list(bounding_region)
 
-    loc = {'topline': True,
-           'baseline': False,
-           'centerline': None}
+        loc = {'topline': True,
+               'baseline': False,
+               'centerline': None}
 
-    rloc = {True: 'topline',
-            False: 'baseline',
-            None: 'centerline'}
-    line_loc = rloc[net.user_metadata.get('topline', False)]
+        rloc = {True: 'topline',
+                False: 'baseline',
+                None: 'centerline'}
+        line_loc = rloc[net.user_metadata.get('topline', False)]
 
-    print(f'Model is {line_loc}')
-    print(f'-> Setting to {topline}')
-    net.user_metadata['topline'] = loc[topline]
+        print(f'Model is {line_loc}')
+        if topline is not None:
+            print(f'-> Setting to {topline}')
+            net.user_metadata['topline'] = loc[topline]
 
-    print(f"Model has padding {net.user_metadata['hyper_params']['padding'] if 'padding' in net.user_metadata['hyper_params'] else (0, 0)}")
-    print(f'-> Setting to {pad}')
-    net.user_metadata['hyper_params']['padding'] = pad
+        hyper_params = net.user_metadata.get('hyper_params', {})
+        print(f"Model has padding {hyper_params.get('padding', (0, 0))}")
+        if pad is not None:
+            print(f'-> Setting to {pad}')
+            net.user_metadata.setdefault('hyper_params', {})['padding'] = pad
 
-    shutil.copy(model, f'{model}.bak')
-    net.save_model(model)
+    path = Path(model)
+    shutil.copy(path, path.with_suffix(path.suffix + '.bak'))
+    path.unlink()
+    writer = write_safetensors if output_format == 'safetensors' else write_coreml
+    writer(all_models, path)
 
 
 if __name__ == '__main__':
