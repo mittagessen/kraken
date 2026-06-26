mittagessen/kraken

commit e4197ffad7f6c20d0305094eff0dc0f6109f1704

Fix an issue with the report of the confusion matrix, where insertions and deletions count were reversed

kraken/lib/dataset/utils.py
@@ -362,13 +362,13 @@ def compute_confusions(algn1: Sequence[str], algn2: Sequence[str]):
     for u, v in zip(algn1, algn2):
         counts[(u, v)] += 1
     for k, v in counts.items():
-        if k[0] == '':
-            dels += v
+        if k[0] == '':  # If there is nothing in algn1, it means it's an insertion in algn2
+            ins += v
         else:
             script = _get_script(k[0])
             scripts[script] += v
-            if k[1] == '':
-                ins[script] += v
+            if k[1] == '':  # Reverse: nothing in k[1] means deletion from k[0]
+                dels[script] += v
             elif k[0] != k[1]:
                 subs[script] += v
     return counts, scripts, ins, dels, subs

commit 980c2cc177858eeb1192e65c9e87ce40b16e1c50

Fix regression causing test report generation crashes
Due to a type mismatch introduced by e4197ffad.

kraken/lib/dataset/utils.py
@@ -340,8 +340,8 @@ def compute_confusions(algn1: Sequence[str], algn2: Sequence[str]):
 
     Returns:
         A tuple (counts, scripts, ins, dels, subs) with `counts` being per-character
-        confusions, `scripts` per-script counts, `ins` a dict with per script
-        insertions, `del` an integer of the number of deletions, `subs` per
+        confusions, `scripts` per-script counts, `ins` an integer of the number
+        of insertions, `dels` a dict with per script deletions, `subs` per
         script substitutions.
     """
     counts: dict[tuple[str, str], int] = Counter()
@@ -355,10 +355,10 @@ def compute_confusions(algn1: Sequence[str], algn2: Sequence[str]):
                 return n
         return 'Unknown'
 
-    scripts: dict[tuple[str, str], int] = Counter()
-    ins: dict[tuple[str, str], int] = Counter()
-    dels: int = 0
-    subs: dict[tuple[str, str], int] = Counter()
+    scripts: dict[str, int] = Counter()
+    ins: int = 0
+    dels: dict[str, int] = Counter()
+    subs: dict[str, int] = Counter()
     for u, v in zip(algn1, algn2):
         counts[(u, v)] += 1
     for k, v in counts.items():

kraken/serialization.py
@@ -277,8 +277,8 @@ def render_report(model: str,
                   word_accuracy: float,
                   char_confusions: 'Counter',
                   scripts: 'Counter',
-                  insertions: 'Counter',
-                  deletions: int,
+                  insertions: int,
+                  deletions: 'Counter',
                   substitutions: 'Counter') -> str:
     """
     Renders an accuracy report.
@@ -289,9 +289,9 @@ def render_report(model: str,
         char_confusions: dictionary mapping a tuple (gt, pred) to a
                                 number of occurrences.
         scripts: dictionary counting character per script.
-        insertions: dictionary counting insertion operations per Unicode
-                           script
-        deletions: Number of deletions
+        insertions: Number of insertions
+        deletions: dictionary counting deletion operations per Unicode
+                          script
         substitutions: dictionary counting substitution operations per
                               Unicode script.
 
@@ -306,13 +306,13 @@ def render_report(model: str,
               'character_accuracy': char_accuracy * 100,
               'character_CI_accucary': char_CI_accucary * 100,
               'word_accuracy': word_accuracy * 100,
-              'insertions': sum(insertions.values()),
-              'deletions': deletions,
+              'insertions': insertions,
+              'deletions': sum(deletions.values()),
               'substitutions': sum(substitutions.values()),
               'scripts': sorted([{'script': k,
                                   'count': v,
-                                  'errors': insertions[k] + substitutions[k],
-                                  'accuracy': 100 * (v - (insertions[k] + substitutions[k])) / v} for k, v in scripts.items()],
+                                  'errors': deletions[k] + substitutions[k],
+                                  'accuracy': 100 * (v - (deletions[k] + substitutions[k])) / v} for k, v in scripts.items()],
                                 key=lambda x: x['accuracy'],
                                 reverse=True),
               'counts': sorted([{'correct': make_printable(k[0]),

kraken/train/utils.py
@@ -80,7 +80,7 @@ class RecognitionTestMetrics(TestMetrics):
     confusions: Counter
     scripts: Counter
     insertions: int
-    deletes: int
+    deletes: Counter
     substitutions: Counter
 
 